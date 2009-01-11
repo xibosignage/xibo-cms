89197de7cd546d56a471d4778f41667ce58e6cd7
Dan Garner <mail@dangarner.co.uk>
2009-01-11 19:05:34 +0000

    [server] Forgot to hide error output in include.php - fixed

--- server/lib/include.php
+++ server/lib/include.php
@@ -22,7 +22,7 @@ defined('XIBO') or die("Sorry, you are not allowed to directly access this page.
 
 // No errors reported until we read the settings from the DB
 error_reporting(0);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('gd.jpeg_ignore_warning', 1);
 
 // Required Library Files
