92375a0193788e428ec85fe8135915f056e76c64
Alex Harrington <alex@longhill.org.uk>
2009-03-11 18:08:39 +0000

    [server] Added function to backup the database programatically

--- server/upgrade.php
+++ server/upgrade.php
@@ -790,6 +790,67 @@ function ls($pattern="*", $folder="", $recursivly=false, $options=array('return_
     return $all;
 }
 
+// Taken from http://davidwalsh.name/backup-mysql-database-php
+// No explicit license. Assumed public domain.
+// If this is your code, and wish for us to remove it, please contact
+// info@xibo.org.uk
+/* backup the db OR just a table */
+function backup_tables($host,$user,$pass,$name,$tables = '*')
+{
+	
+	$link = mysql_connect($host,$user,$pass);
+	mysql_select_db($name,$link);
+	
+	//get all of the tables
+	if($tables == '*')
+	{
+		$tables = array();
+		$result = mysql_query('SHOW TABLES');
+		while($row = mysql_fetch_row($result))
+		{
+			$tables[] = $row[0];
+		}
+	}
+	else
+	{
+		$tables = is_array($tables) ? $tables : explode(',',$tables);
+	}
+	
+	//cycle through
+	foreach($tables as $table)
+	{
+		$result = mysql_query('SELECT * FROM '.$table);
+		$num_fields = mysql_num_fields($result);
+		
+		$return.= 'DROP TABLE '.$table.';';
+		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$return.= "\n\n".$row2[1].";\n\n";
+		
+		for ($i = 0; $i < $num_fields; $i++) 
+		{
+			while($row = mysql_fetch_row($result))
+			{
+				$return.= 'INSERT INTO '.$table.' VALUES(';
+				for($j=0; $j<$num_fields; $j++) 
+				{
+					$row[$j] = addslashes($row[$j]);
+					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+					if ($j<($num_fields-1)) { $return.= ','; }
+				}
+				$return.= ");\n";
+			}
+		}
+		$return.="\n\n\n";
+	}
+	
+	//save file
+	$handle = fopen('db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
+	fwrite($handle,$return);
+	fclose($handle);
+}
+
+
 function gen_secret() {
   # Generates a random 12 character alphanumeric string to use as a salt
   mt_srand((double)microtime()*1000000);
