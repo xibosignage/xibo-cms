1b6c8aba17a20600a2ae57b5e5a4edd9d0d588a6
Alex Harrington <alex@longhill.org.uk>
2009-03-11 18:15:11 +0000

    [server] Ammended backup function to use the database class, and also added a function to module_db_mysql.php to query the number of fields returned by a query.

--- server/upgrade.php
+++ server/upgrade.php
@@ -792,21 +792,18 @@ function ls($pattern="*", $folder="", $recursivly=false, $options=array('return_
 
 // Taken from http://davidwalsh.name/backup-mysql-database-php
 // No explicit license. Assumed public domain.
+// Ammended to use a database object by Alex Harrington.
 // If this is your code, and wish for us to remove it, please contact
 // info@xibo.org.uk
 /* backup the db OR just a table */
-function backup_tables($host,$user,$pass,$name,$tables = '*')
+function backup_tables($db,$tables = '*')
 {
-	
-	$link = mysql_connect($host,$user,$pass);
-	mysql_select_db($name,$link);
-	
 	//get all of the tables
 	if($tables == '*')
 	{
 		$tables = array();
-		$result = mysql_query('SHOW TABLES');
-		while($row = mysql_fetch_row($result))
+		$result = $db->query('SHOW TABLES');
+		while($row = $db->get_row($result))
 		{
 			$tables[] = $row[0];
 		}
@@ -819,16 +816,16 @@ function backup_tables($host,$user,$pass,$name,$tables = '*')
 	//cycle through
 	foreach($tables as $table)
 	{
-		$result = mysql_query('SELECT * FROM '.$table);
-		$num_fields = mysql_num_fields($result);
+		$result = $db->query('SELECT * FROM '.$table);
+		$num_fields = $db->num_fields($result);
 		
-		$return.= 'DROP TABLE '.$table.';';
-		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$return.= 'DROP TABLE IF EXISTS '.$table.';';
+		$row2 = $db->get_row($db->query('SHOW CREATE TABLE '.$table));
 		$return.= "\n\n".$row2[1].";\n\n";
 		
 		for ($i = 0; $i < $num_fields; $i++) 
 		{
-			while($row = mysql_fetch_row($result))
+			while($row = $db->get_row($result))
 			{
 				$return.= 'INSERT INTO '.$table.' VALUES(';
 				for($j=0; $j<$num_fields; $j++) 
@@ -845,7 +842,7 @@ function backup_tables($host,$user,$pass,$name,$tables = '*')
 	}
 	
 	//save file
-	$handle = fopen('db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
+	$handle = fopen(Config::GetSetting($db,'LIBRARY_LOCATION') . 'db-backup-'.time().'-'.(md5(implode(',',$tables))).'.sql','w+');
 	fwrite($handle,$return);
 	fclose($handle);
 }
