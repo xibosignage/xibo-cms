131dc97ffacf7c2fe7fdfbccfceb5170c544fec9
Dan Garner <mail@dangarner.co.uk>
2009-09-19 14:01:49 +0100

    [server] Upgrade: Code to create a displaygroup for each display

--- server/install/database/20.php
+++ server/install/database/20.php
@@ -11,7 +11,27 @@ class Step20 extends UpgradeStep
 		
 		
 		// TODO: Will need to add some upgrade PHP to create a DisplayGroup (+ link record) for every Currently existing display.
+		$dg = new DisplayGroup($db);
+
+		// Get all displays
+		$SQL = "SELECT DisplayID, Display FROM display";
+		
+		if (!$result = $db->query($SQL))
+		{
+			trigger_error("Error creating display groups", E_USER_ERROR);
+		}
+		
+		while ($row = $db->get_assoc_row($result))
+		{
+			// For each display create a display group and link it to the display
+			$displayID		= Kit::ValidateParam($row['DisplayID'], _INT);
+			$display		= Kit::ValidateParam($row['Display'], _STRING);
 			
+			$displayGroupID	= $dg->Add($display, 1);
+			
+			$dg->Link($displayGroupID, $displayID);
+		}
+		
 		return true;
 	}
 
