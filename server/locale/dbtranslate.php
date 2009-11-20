d6755ea7d7c636f128fde2850acb73cd37c44330
Dan Garner <dan@xibo.org.uk>
2009-11-20 13:34:32 +0000

    [server] Added menu item strings to a dummy file so they are translated. Translated the menu when outputting it. Updated the POT file.

---
+++ server/locale/dbtranslate.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Xibo - Digitial Signage - http://www.xibo.org.uk
+ * Copyright (C) 2009 Daniel Garner
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+defined('XIBO') or die("Sorry, you are not allowed to directly access this page.<br /> Please press the back button in your browser.");
+
+/* Translations from the Database that need to be registered with Gettext */
+
+echo __('Schedule');
+echo __('Layout');
+echo __('Library');
+echo __('Management');
+echo __('Displays');
+echo __('Groups');
+echo __('Users');
+echo __('Log');
+echo __('License');
+echo __('Sessions');
+echo __('Settings');
+echo __('Schedule');
+echo __('Layouts');
+echo __('Library');
+echo __('Templates');
+echo __('Settings');
+echo __('Edit your Display');
+echo __('Report Fault');
+echo __('Statistics');
+echo __('Display Groups');
+
+?>
