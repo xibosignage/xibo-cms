3b42ccbc8256d446b3aa44df61f5c9eecac5d513
Daniel Garner <dan@xibo.org.uk>
2009-12-31 11:29:53 +0000

    [server] Incremented Server Version.

--- server/install/database/9.sql
+++ server/install/database/9.sql
@@ -11,4 +11,8 @@ INSERT INTO `menuitem` (`MenuItemID`, `MenuID`, `PageID`, `Args`, `Text`, `Class
 VALUES (NULL, 2, 28, 'http://wiki.xibo.org.uk/wiki/Manual:TOC', 'Manual', 'help_button', 'img/dashboard/help.png', '10');
 
 
-UPDATE `module` SET `ValidExtensions` = 'ppt,pps,pptx' WHERE `module`.`ModuleID` =4 LIMIT 1 ;>>>>>>> MERGE-SOURCE
+UPDATE `module` SET `ValidExtensions` = 'ppt,pps,pptx' WHERE `module`.`ModuleID` =4 LIMIT 1 ;
+
+UPDATE `version` SET `app_ver` = '1.0.5';
+UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
+UPDATE `version` SET `DBVersion` = '9';
