66c2c0db3bac895d0f1b14919d0b9b5b56082589
Dan Garner <dan@xibo.org.uk>
2010-02-21 18:55:44 +0000

    [server] Missed a file.

---
+++ server/template/pages/oauth_verify.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Xibo - Digitial Signage - http://www.xibo.org.uk
+ * Copyright (C) 2010 Daniel Garner
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+defined('XIBO') or die("Sorry, you are not allowed to directly access this page.<br /> Please press the back button in your browser.");
+
+?>
+<div id="form_container">
+    <div id="form_header">
+        <div id="form_header_left"></div>
+        <div id="form_header_right"></div>
+    </div>
+
+    <div id="form_body">
+
+        <div>
+            <h1>Authorized Web Services</h1>
+            <form method="post">
+                <input type="submit" value="Allow">
+            </form>
+        </div>
+    </div>
+
+    <div id="form_footer">
+        <div id="form_footer_left"></div>
+        <div id="form_footer_right"></div>
+    </div>
+</div>
