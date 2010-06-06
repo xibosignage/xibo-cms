b1b9af8ada4feafeab43650bac4214ec94a6d150
Alex Harrington <alex@longhill.org.uk>
2010-06-06 01:58:50 +0100

    [server] Renamed MAINTENANCE_ALERT_TIMEOUT to MAINTENANCE_ALERT_TOUT to avoid the setting name being truncated.

--- server/install/database/30.sql
+++ server/install/database/30.sql
@@ -4,7 +4,7 @@ INSERT INTO `setting` (`setting`, `value`, `type`, `helptext`, `options`, `cat`,
   ('MAINTENANCE_EMAIL_ALERTS', 'On', 'dropdown', 'Global switch for email alerts to be sent','On|Off','maintenance','1'),
   ('MAINTENANCE_LOG_MAXAGE', '30', 'text', 'Maximum age for log entries. Set to 0 to keep logs indefinitely.',NULL,'maintenance','1'),
   ('MAINTENANCE_STAT_MAXAGE', '30', 'text', 'Maximum age for statistics entries. Set to 0 to keep statistics indefinitely.',NULL,'maintenance','1'),
-  ('MAINTENANCE_ALERT_TIMEOUT', '12', 'text', 'How long in minutes after the last time a client connects should we send an alert? Can be overridden on a per client basis.',NULL,'maintenance','1');
+  ('MAINTENANCE_ALERT_TOUT', '12', 'text', 'How long in minutes after the last time a client connects should we send an alert? Can be overridden on a per client basis.',NULL,'maintenance','1');
 
 /* Unhide the email settings that exist already and move them to the maintenance tab */
 UPDATE `setting` SET `cat` = 'maintenance', `userChange` = '1' WHERE (`setting` = 'mail_to' OR `setting` = 'mail_from');
