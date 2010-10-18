b33d06ef2eb432ce781137941d3019a86942c46b
Alex Harrington <alex@longhill.org.uk>
2010-10-18 18:21:37 +0100

    [server] Hopefully fixed for both Apache and IIS servers.

--- server/lib/app/kit.class.php
+++ server/lib/app/kit.class.php
@@ -425,7 +425,6 @@ class Kit
         # Copyright (c) 2009, Ecenica Limited All rights reserved.
         if (!isset($_SERVER['REQUEST_URI']))
         {
-            $_SERVER['REQUEST_URI'] = substr($_SERVER['PHP_SELF'],1 );
             if (isset($_SERVER['QUERY_STRING']))
             {
                 $_SERVER['REQUEST_URI'].='?'.$_SERVER['QUERY_STRING'];
@@ -453,7 +452,7 @@ class Kit
             $fullUrl .=  $_SERVER['SERVER_NAME'];
         }
 
-        return $fullUrl . '/' . $request[0];
+        return $fullUrl . $request[0];
     }
 
     /**
@@ -469,7 +468,6 @@ class Kit
         # Copyright (c) 2009, Ecenica Limited All rights reserved.
         if (!isset($_SERVER['REQUEST_URI']))
         {
-            $_SERVER['REQUEST_URI'] = substr($_SERVER['PHP_SELF'],1 );
             if (isset($_SERVER['QUERY_STRING']))
             {
                 $_SERVER['REQUEST_URI'].='?'.$_SERVER['QUERY_STRING'];
