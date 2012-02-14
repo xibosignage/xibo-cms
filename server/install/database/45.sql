a57b2ebc962f160aef6a0a955e04c1b30a6c43a6
Dan Garner <dan@xibo.org.uk>
2012-02-14 13:40:41 +0000

    [server] Display table column bug - no default

--- server/install/database/45.sql
+++ server/install/database/45.sql
@@ -8,6 +8,8 @@ VALUES (
 NULL , 'User', 'ChangePassword', 'http://wiki.xibo.org.uk/wiki/Manual:Administration:Users#Change_Password'
 );
 
+ALTER TABLE `display` CHANGE `NumberOfMacAddressChanges` `NumberOfMacAddressChanges` INT( 11 ) NOT NULL DEFAULT '0';
+
 UPDATE `version` SET `app_ver` = '1.3.3', `XmdsVersion` = 3;
 UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
 UPDATE `version` SET `DBVersion` = '45';
