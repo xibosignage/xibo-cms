f3f23b03cae59ebbd5f92d067aea60f940e47d04
Dan Garner <dan@xibo.org.uk>
2012-05-04 23:23:01 +0100

    [server] Upgrade step to clean and reenter all help links

--- server/lib/include.php
+++ server/lib/include.php
@@ -108,8 +108,8 @@ set_error_handler(array(new Debug(), "ErrorHandler"));
 Config::Version($db);
 
 // Does the version in the DB match the version of the code?
-if (DBVERSION != '44')
-    die(sprintf('Incompatible database version detected. Please ensure your database and website versions match. You have %d and the website for %d', DBVERSION, 44));
+if (DBVERSION != '45')
+    die(sprintf('Incompatible database version detected. Please ensure your database and website versions match. You have %d and the website for %d', DBVERSION, 45));
 
 // What is the production mode of the server?
 if(Config::GetSetting($db, 'SERVER_MODE') == 'Test') ini_set('display_errors', 1);
