130214964349a5ca7ebdacf272a79c1d850797be
Dan Garner <dan@xibo.org.uk>
2012-05-06 16:16:31 +0100

    [server] Begin work on Campaigns

--- server/install/database/45.sql
+++ server/install/database/45.sql
@@ -155,6 +155,29 @@ ALTER TABLE `schedule_detail` ADD FOREIGN KEY (  `DisplayGroupID` ) REFERENCES `
 `DisplayGroupID`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
 
+CREATE TABLE IF NOT EXISTS `campaign` (
+  `CampaignID` int(11) NOT NULL AUTO_INCREMENT,
+  `Campaign` varchar(254) NOT NULL,
+  `IsLayoutSpecific` tinyint(4) NOT NULL,
+  PRIMARY KEY (`CampaignID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `lkcampaigngroup` (
+  `LkCampaignGroupID` int(11) NOT NULL AUTO_INCREMENT,
+  `CampaignID` int(11) NOT NULL,
+  `GroupID` int(11) NOT NULL,
+  `View` tinyint(4) NOT NULL DEFAULT '0',
+  `Edit` tinyint(4) NOT NULL DEFAULT '0',
+  `Del` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`LkCampaignGroupID`),
+  KEY `CampaignID` (`CampaignID`),
+  KEY `GroupID` (`GroupID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `lkcampaigngroup`
+  ADD CONSTRAINT `lkcampaigngroup_ibfk_2` FOREIGN KEY (`GroupID`) REFERENCES `group` (`groupID`),
+  ADD CONSTRAINT `lkcampaigngroup_ibfk_1` FOREIGN KEY (`CampaignID`) REFERENCES `campaign` (`CampaignID`);
+
 UPDATE `version` SET `app_ver` = '1.3.3', `XmdsVersion` = 3;
 UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
 UPDATE `version` SET `DBVersion` = '45';
