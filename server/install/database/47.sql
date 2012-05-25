ba0c37236c6159187ae28447687215a6d12bb599
Dan Garner <dan@xibo.org.uk>
2012-05-25 19:44:46 +0100

    [server] New module, shell command. Used to execute a command in the shell on the client.

--- server/install/database/47.sql
+++ server/install/database/47.sql
@@ -12,6 +12,8 @@ ALTER TABLE  `schedule` ADD FOREIGN KEY (  `CampaignID` ) REFERENCES  `campaign`
 
 DROP TABLE lklayoutgroup;
 
+INSERT INTO `module` (`ModuleID`, `Module`, `Name`, `Enabled`, `RegionSpecific`, `Description`, `ImageUri`, `SchemaVersion`, `ValidExtensions`) VALUES (NULL, 'shellcommand', 'Shell Command', '1', '1', 'Execute a shell command on the client', 'img/forms/shellcommand.gif', '1', NULL);
+
 UPDATE `version` SET `app_ver` = '1.3.3', `XmdsVersion` = 3;
 UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
 UPDATE `version` SET `DBVersion` = '47';
---
+++ server/modules/shellcommand.module.php
@@ -0,0 +1,263 @@
+<?php
+/*
+ * Xibo - Digitial Signage - http://www.xibo.org.uk
+ * Copyright (C) 2012 Daniel Garner
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+class shellcommand extends Module
+{
+	
+    public function __construct(database $db, user $user, $mediaid = '', $layoutid = '', $regionid = '', $lkid = '')
+    {
+        // Must set the type of the class
+        $this->type = 'shellcommand';
+        $this->displayType = 'Shell Command';
+
+        // Must call the parent class
+        parent::__construct($db, $user, $mediaid, $layoutid, $regionid, $lkid);
+    }
+	
+    /**
+     * Return the Add Form as HTML
+     * @return
+     */
+    public function AddForm()
+    {
+        $db =& $this->db;
+        $user =& $this->user;
+        $layoutid = $this->layoutid;
+        $regionid = $this->regionid;
+        
+        $msgWindows = __('Windows Command');
+        $msgLinux = __('Linux Command');
+
+        $form = <<<FORM
+        <form id="ModuleForm" class="XiboForm" method="post" action="index.php?p=module&mod=$this->type&q=Exec&method=AddMedia">
+            <input type="hidden" name="layoutid" value="$layoutid">
+            <input type="hidden" id="iRegionId" name="regionid" value="$regionid">
+            <input type="hidden" name="showRegionOptions" value="$this->showRegionOptions" />
+            <table>
+                <tr>
+                    <td><label for="windowsCommand" title="">$msgWindows</label></td>
+                    <td><input id="windowsCommand" name="windowsCommand" type="text"></td>
+                </tr>
+                <tr>
+                    <td><label for="linuxCommand" title="">$msgLinux</label></td>
+                    <td><input id="linuxCommand" name="linuxCommand" type="text"></td>
+                </tr>
+            </table>
+        </form>
+FORM;
+
+        $this->response->html = $form;
+        if ($this->showRegionOptions)
+        {
+            $this->response->AddButton(__('Cancel'), 'XiboSwapDialog("index.php?p=layout&layoutid=' . $layoutid . '&regionid=' . $regionid . '&q=RegionOptions")');
+        }
+        else
+        {
+            $this->response->AddButton(__('Cancel'), 'XiboDialogClose()');
+        }
+        $this->response->AddButton(__('Save'), '$("#ModuleForm").submit()');
+        $this->response->dialogTitle = __('Add Shell Command');
+        $this->response->dialogSize 	= true;
+        $this->response->dialogWidth 	= '450px';
+        $this->response->dialogHeight 	= '250px';
+
+        return $this->response;
+    }
+	
+    /**
+     * Return the Edit Form as HTML
+     * @return
+     */
+    public function EditForm()
+    {
+        $db =& $this->db;
+        $layoutid = $this->layoutid;
+        $regionid = $this->regionid;
+        $mediaid = $this->mediaid;
+
+        // Permissions
+        if (!$this->auth->edit)
+        {
+            $this->response->SetError('You do not have permission to edit this assignment.');
+            $this->response->keepOpen = true;
+            return $this->response;
+        }
+		
+        $windowsCommand = $this->GetOption('windowsCommand');
+        $linuxCommand = $this->GetOption('linuxCommand');
+
+        $msgWindows = __('Windows Command');
+        $msgLinux = __('Linux Command');
+
+        $form = <<<FORM
+        <form id="ModuleForm" class="XiboForm" method="post" action="index.php?p=module&mod=$this->type&q=Exec&method=EditMedia">
+            <input type="hidden" name="layoutid" value="$layoutid">
+            <input type="hidden" id="iRegionId" name="regionid" value="$regionid">
+            <input type="hidden" name="mediaid" value="$mediaid">
+            <input type="hidden" name="showRegionOptions" value="$this->showRegionOptions" />
+            <table>
+                <tr>
+                    <td><label for="windowsCommand" title="">$msgWindows</label></td>
+                    <td><input id="windowsCommand" name="windowsCommand" type="text" value="$windowsCommand"></td>
+                </tr>
+                <tr>
+                    <td><label for="linuxCommand" title="">$msgLinux</label></td>
+                    <td><input id="linuxCommand" name="linuxCommand" type="text" value="$linuxCommand"></td>
+                </tr>
+            </table>
+        </form>
+FORM;
+
+        $this->response->html = $form;
+
+        if ($this->showRegionOptions)
+        {
+            $this->response->AddButton(__('Cancel'), 'XiboSwapDialog("index.php?p=layout&layoutid=' . $layoutid . '&regionid=' . $regionid . '&q=RegionOptions")');
+        }
+        else
+        {
+            $this->response->AddButton(__('Cancel'), 'XiboDialogClose()');
+        }
+
+        $this->response->AddButton(__('Save'), '$("#ModuleForm").submit()');
+        $this->response->dialogTitle = __('Edit Shell Command');
+        $this->response->dialogSize 	= true;
+        $this->response->dialogWidth 	= '450px';
+        $this->response->dialogHeight 	= '250px';
+
+        return $this->response;
+    }
+	
+    /**
+     * Add Media to the Database
+     * @return
+     */
+    public function AddMedia()
+    {
+        $db =& $this->db;
+
+        $layoutid = $this->layoutid;
+        $regionid = $this->regionid;
+        $mediaid = $this->mediaid;
+
+        // Required Attributes
+        $this->mediaid = md5(uniqid());
+        $this->duration = 1;
+        
+        $windowsCommand = Kit::GetParam('windowsCommand', _POST, _STRING);
+        $linuxCommand = Kit::GetParam('linuxCommand', _POST, _STRING);
+
+        if ($windowsCommand == '' && $linuxCommand == '')
+        {
+            $this->response->SetError('You must enter a command');
+            $this->response->keepOpen = true;
+            return $this->response;
+        }
+
+        // Any Options
+        $this->SetOption('windowsCommand', $windowsCommand);
+        $this->SetOption('linuxCommand', $linuxCommand);
+
+        // Should have built the media object entirely by this time
+        // This saves the Media Object to the Region
+        $this->UpdateRegion();
+
+        // Set this as the session information
+        setSession('content', 'type', 'shellcommand');
+
+        if ($this->showRegionOptions)
+        {
+            // We want to load a new form
+            $this->response->loadForm = true;
+            $this->response->loadFormUri = "index.php?p=layout&layoutid=$layoutid&regionid=$regionid&q=RegionOptions";
+        }
+
+        return $this->response;
+    }
+
+    /**
+     * Edit Media in the Database
+     * @return
+     */
+    public function EditMedia()
+    {
+        $db =& $this->db;
+
+        $layoutid = $this->layoutid;
+        $regionid = $this->regionid;
+        $mediaid = $this->mediaid;
+
+        if (!$this->auth->edit)
+        {
+            $this->response->SetError('You do not have permission to edit this assignment.');
+            $this->response->keepOpen = false;
+            return $this->response;
+        }
+
+        $windowsCommand = Kit::GetParam('windowsCommand', _POST, _STRING);
+        $linuxCommand = Kit::GetParam('linuxCommand', _POST, _STRING);
+
+        if ($windowsCommand == '' && $linuxCommand == '')
+        {
+            $this->response->SetError('You must enter a command');
+            $this->response->keepOpen = true;
+            return $this->response;
+        }
+
+        // Any Options
+        $this->duration = 1;
+        $this->SetOption('windowsCommand', $windowsCommand);
+        $this->SetOption('linuxCommand', $linuxCommand);
+
+        // Should have built the media object entirely by this time
+        // This saves the Media Object to the Region
+        $this->UpdateRegion();
+
+        // Set this as the session information
+        setSession('content', 'type', 'shellcommand');
+
+        if ($this->showRegionOptions)
+        {
+            // We want to load a new form
+            $this->response->loadForm = true;
+            $this->response->loadFormUri = "index.php?p=layout&layoutid=$layoutid&regionid=$regionid&q=RegionOptions";
+        }
+
+        return $this->response;
+    }
+
+    public function Preview()
+    {
+        $msgWindows = __('Windows Command');
+        $msgLinux = __('Linux Command');
+
+        $preview = '';
+        $preview .= '<p>' . $msgWindows . ': ' . $this->GetOption('windowsCommand') . '</p>';
+        $preview .= '<p>' . $msgLinux . ': ' . $this->GetOption('linuxCommand') . '</p>';
+
+        return $preview;
+    }
+
+    public function HoverPreview()
+    {
+        return $this->Preview();
+    }
+}
+?>
