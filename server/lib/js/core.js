fe72506dbb9924ee2697c5c85f65f2f7fb09aaed
Dan Garner <dan@xibo.org.uk>
2012-09-27 22:48:43 +0100

    [server] Fix for filter forms with out keep open checkboxes

--- server/lib/js/core.js
+++ server/lib/js/core.js
@@ -84,11 +84,13 @@ function XiboInitialise(scope){
         var gridId = $(this).attr("id");
         
         // Keep this filter form open?
-        if ($('.XiboFilter form :input.XiboFilterPinned', this).is(':checked')) {
-            $('.XiboFilter', this).children(':first').show();
-        }
-        else {
-            $('.XiboFilter', this).children(':first').hide();
+        if ($('.XiboFilter form :input.XiboFilterPinned', this).length > 0) {
+            if ($('.XiboFilter form :input.XiboFilterPinned', this).is(':checked')) {
+                $('.XiboFilter', this).children(':first').show();
+            }
+            else {
+                $('.XiboFilter', this).children(':first').hide();
+            }
         }
         
         // For each one setup the filter form bindings
