e26b92d0309e7256ffa3165e73d5b991b3fb462b
Dan Garner <dan@xibo.org.uk>
2013-03-25 17:31:19 +0000

    [server] Fixed data.sql

--- server/install/master/data.sql
+++ server/install/master/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version` (`app_ver`, `XmdsVersion`, `XlfVersion`, `DBVersion`) VALUES
-('1.5.0', 3, 1, 61);
+('1.5.0', 3, 1, 62);
 
 INSERT INTO `group` (`groupID`, `group`, `IsUserSpecific`, `IsEveryone`) VALUES
 (1, 'Users', 0, 0),
