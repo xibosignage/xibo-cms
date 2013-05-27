5d88ac3e34353b2dd8b71ca77996759fcd1ef1c9
Dan Garner <dan@xibo.org.uk>
2013-05-27 17:20:53 +0100

    [server] Extra attributes allowed on tables in CKEditor

--- server/theme/default/libraries/ckeditor/config.js
+++ server/theme/default/libraries/ckeditor/config.js
@@ -37,4 +37,5 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
 	config.extraPlugins = 'iframe';
+	config.extraAllowedContent = 'table[*]';
 };
