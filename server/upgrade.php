00a4a6cd4de22f5f0ac1b16386275c5cacf0a2ec
Dan Garner <dan@xibo.org.uk>
2013-11-06 15:10:13 +0000

    [server] Missing require_once("lib/app/pdoconnect.class.php"); from upgrade.php

--- server/upgrade.php
+++ server/upgrade.php
@@ -27,6 +27,7 @@ if (! checkPHP()) {
 error_reporting(0);
 ini_set('display_errors', 0);
 
+require_once("lib/app/pdoconnect.class.php");
 include('lib/app/kit.class.php');
 include("lib/data/data.class.php");
 include('lib/app/debug.class.php');
