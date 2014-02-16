6a0ca21a740b91bf9c692e5ed94305e5e5b8d22f
Dan Garner <dan@xibo.org.uk>
2014-02-16 18:13:35 +0000

    [cms] Pushed version for 1.6.0 and DB 67

--- server/install/master/data.sql
+++ server/install/master/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `version` (`app_ver`, `XmdsVersion`, `XlfVersion`, `DBVersion`) VALUES
-('1.6.0-rc1', 3, 1, 66);
+('1.6.0', 3, 1, 67);
 
 INSERT INTO `group` (`groupID`, `group`, `IsUserSpecific`, `IsEveryone`) VALUES
 (1, 'Users', 0, 0),
