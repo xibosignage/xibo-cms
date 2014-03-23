4e9310040c80f6db52ca250c272baf000b7f1a32
Dan Garner <dan@xibo.org.uk>
2014-03-23 13:24:51 +0000

    [cms] Fix compatibilty bug

---
+++ server/theme/default/libraries/bootstrap/js/bootstrap-ckeditor-fix.js
@@ -0,0 +1,15 @@
+// bootstrap-ckeditor-fix.js
+// hack to fix ckeditor/bootstrap compatiability bug when ckeditor appears in a bootstrap modal dialog
+//
+// Include this file AFTER both jQuery and bootstrap are loaded.
+
+$.fn.modal.Constructor.prototype.enforceFocus = function() {
+  modal_this = this
+  $(document).on('focusin.modal', function (e) {
+    if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length 
+    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select') 
+    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+      modal_this.$element.focus()
+    }
+  })
+};
