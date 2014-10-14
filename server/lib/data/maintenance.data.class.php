3999ee1ab084e377854212c2d2884926cd33b70c
Dan Garner <dan@xibo.org.uk>
2014-10-14 16:05:24 +0100

    [cms] Library tidy enhancement to delete unused media revisions.

--- server/lib/data/maintenance.data.class.php
+++ server/lib/data/maintenance.data.class.php
@@ -69,5 +69,95 @@ class Maintenance extends Data
 
         return true;
     }
+
+    public function TidyLibrary($tidyOldRevisions) {
+        // Also run a script to tidy up orphaned media in the library
+        $library = Config::GetSetting('LIBRARY_LOCATION');
+        $library = rtrim($library, '/') . '/';
+        $mediaObject = new Media();
+
+        Debug::Audit('Library Location: ' . $library);
+
+        // Dump the files in the temp folder
+        foreach (scandir($library . 'temp') as $item) {
+            if ($item == '.' || $item == '..')
+                continue;
+
+            Debug::Audit('Deleting temp file: ' . $item);
+
+            unlink($library . 'temp' . DIRECTORY_SEPARATOR . $item);
+        }
+
+        $media = array();
+        $unusedMedia = array();
+
+        // Run a query to get an array containing all of the media in the library
+        try {
+            $dbh = PDOConnect::init();
+        
+            $sth = $dbh->prepare('
+                SELECT media.mediaid, media.storedAs, media.type, media.isedited, COUNT(lklayoutmedia.lklayoutmediaid) AS UsedInLayoutCount 
+                  FROM `media` 
+                    LEFT OUTER JOIN `lklayoutmedia`
+                    ON lklayoutmedia.mediaid = media.mediaid
+                GROUP BY media.mediaid, media.storedAs ');
+
+            $sth->execute(array());
+
+            foreach ($sth->fetchAll() as $row) {
+                $media[$row['storedAs']] = $row;
+                
+                // If its not used in a layout and its not a generic module, add to the unused array.
+                if ($tidyOldRevisions && $row['UsedInLayoutCount'] <= 0 && $row['isedited'] > 0 && $row['type'] != 'module' && $row['type'] != 'font')
+                    $unusedMedia[$row['storedAs']] = $row;
+            }
+        }
+        catch (Exception $e) {
+            
+            Debug::LogEntry('error', $e->getMessage());
+        
+            if (!$this->IsError())
+                $this->SetError(1, __('Unknown Error'));
+        
+            return false;
+        }
+
+        //Debug::Audit(var_export($media, true));
+        //Debug::Audit(var_export($unusedMedia, true));
+
+        // Get a list of all media files
+        foreach(scandir($library) as $file) {
+
+            if ($file == '.' || $file == '..')
+                continue;
+
+            if (is_dir($library . $file))
+                continue;
+
+            // Ignore thumbnails
+            if (strstr($file, 'tn_') || strstr($file, 'bg_'))
+                continue;
+            
+            // Is this file in the system anywhere?
+            if (!array_key_exists($file, $media)) {
+                // Totally missing
+                Debug::Audit('Deleting file: ' . $file);
+                
+                // If not, delete it
+                $mediaObject->DeleteMediaFile($file);
+            }
+            else if (array_key_exists($file, $unusedMedia)) {
+                // It exists but isn't being used any more
+                Debug::Audit('Deleting media: ' . $media[$file]['mediaid']);
+                $mediaObject->Delete($media[$file]['mediaid']);
+            }
+            else {
+                // Don't do anything, this file still exists
+                //Debug::Audit('Still exists: ' . $file);
+            }
+        }
+
+        return true;
+    }
 }
 ?>
