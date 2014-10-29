3df01c4719dfdbab4e660b5dfd55ba1e9572df04
Dan Garner <dan@xibo.org.uk>
2014-10-29 14:11:36 +0000

    [manual] Moved and mgrated more pages.

--- server/manual/content/admin/api_displays.php
+++
@@ -1,8 +0,0 @@
-<h3> <span class="mw-headline" id="Displays"> Displays </span></h3>
-<ul><li> DisplayList
-</li><li> DisplayEdit
-</li><li> DisplayRetire
-</li><li> DisplayDelete
-</li><li> DisplayUserGroupSecurity
-</li><li> DisplayUserGroupEdit
-</li></ul>
--- server/manual/content/admin/api_resolution.php
+++
@@ -1,4 +0,0 @@
-
-<h3> <span class="mw-headline" id="Resolution"> Resolution </span></h3>
-<ul><li> ResolutionList
-</li></ul>
