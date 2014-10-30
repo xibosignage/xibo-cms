22cf61e0b812fffe82319b01b9e80a9050a51079
Dan Garner <dan@xibo.org.uk>
2014-10-30 12:31:37 +0000

    [server] Removed old manual

--- server/manual/content/install/troubleshooting.php
+++
@@ -1,7 +0,0 @@
-
-	<h1>Troubleshooting</h1>
-
-	<p>Please head over to our Support page if you have problems installing <?php echo PRODUCT_NAME; ?>.</p>
-	<p><a href="<?php echo PRODUCT_SUPPORT_URL; ?>" target="_blank"><?php echo PRODUCT_SUPPORT_URL; ?></a>
-	<p>Be sure to checkout the FAQs too.</p>
-	<p><a href="<?php echo PRODUCT_FAQ_URL; ?>" target="_blank"><?php echo PRODUCT_FAQ_URL; ?></a></p>
