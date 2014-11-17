839934e2753fe9969e1f43edee6068085d986cb1
Dan Garner <dan@xibo.org.uk>
2014-11-17 10:36:17 +0000

    [cms] Option to set mute on video

--- server/modules/video.module.php
+++ server/modules/video.module.php
@@ -74,14 +74,21 @@ class video extends Module
     {
         $this->response = new ResponseManager();
         $formFields = array();
-        $formFields[] = FormManager::AddCheckbox('loop', __('Loop?'), 
-            $this->GetOption('loop', 0), __('Should the video loop if it finishes before the provided duration?'), 
-            'l', 'loop-fields');
 
-        $this->response->AddFieldAction('duration', 'init', '0', array('.loop-fields' => array('display' => 'none')));
-        $this->response->AddFieldAction('duration', 'change', '0', array('.loop-fields' => array('display' => 'none')));
-        $this->response->AddFieldAction('duration', 'init', '0', array('.loop-fields' => array('display' => 'block')), 'not');
-        $this->response->AddFieldAction('duration', 'change', '0', array('.loop-fields' => array('display' => 'block')), 'not');
+        if ($this->layoutid != '' && $this->regionid != '') {
+            $formFields[] = FormManager::AddCheckbox('loop', __('Loop?'), 
+                $this->GetOption('loop', 0), __('Should the video loop if it finishes before the provided duration?'), 
+                'l', 'loop-fields');
+
+            $formFields[] = FormManager::AddCheckbox('mute', __('Mute?'), 
+                $this->GetOption('mute', 1), __('Should the video be muted?'), 
+                'm', 'mute-fields');
+
+            $this->response->AddFieldAction('duration', 'init', '0', array('.loop-fields' => array('display' => 'none')));
+            $this->response->AddFieldAction('duration', 'change', '0', array('.loop-fields' => array('display' => 'none')));
+            $this->response->AddFieldAction('duration', 'init', '0', array('.loop-fields' => array('display' => 'block')), 'not');
+            $this->response->AddFieldAction('duration', 'change', '0', array('.loop-fields' => array('display' => 'block')), 'not');
+        }
 
         return $this->EditFormForLibraryMedia($formFields);
     }
@@ -103,6 +110,7 @@ class video extends Module
     {
         // Set the properties specific to Images
         $this->SetOption('loop', Kit::GetParam('loop', _POST, _CHECKBOX));
+        $this->SetOption('mute', Kit::GetParam('mute', _POST, _CHECKBOX));
 
         return $this->EditLibraryMedia();
     }
