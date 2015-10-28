4ff14056974adf52650dc29796d6eda0d5592046
Dan Garner <dan@springsignage.com>
2015-10-28 14:35:14 +0000

    Tweaked stats so that the date filter works.

--- lib/Controller/Stats.php
+++ lib/Controller/Stats.php
@@ -138,8 +138,8 @@ class Stats extends Base
      */
     public function grid()
     {
-        $fromDt = Sanitize::getDate('fromDt', Date::parse()->addDay(-1));
-        $toDt = Sanitize::getDate('toDt', Date::parse());
+        $fromDt = Sanitize::getDate('fromDt', Sanitize::getDate('statsFromDt', Date::parse()->addDay(-1)));
+        $toDt = Sanitize::getDate('toDt', Sanitize::getDate('statsToDt', Date::parse()));
         $displayId = Sanitize::getInt('displayId');
         $mediaId = Sanitize::getInt('mediaId');
 
@@ -166,7 +166,7 @@ class Stats extends Base
           SELECT stat.type,
               display.Display,
               layout.Layout,
-              IFNULL(widgetoption.value, widget.type) AS Name,
+              IFNULL(widgetoption.value, CONCAT(widget.type, \'-\', widget.widgetId)) AS Name,
               COUNT(StatID) AS NumberPlays,
               SUM(TIME_TO_SEC(TIMEDIFF(end, start))) AS Duration,
               MIN(start) AS MinStart,
@@ -176,11 +176,11 @@ class Stats extends Base
               ON stat.DisplayID = display.DisplayID
               INNER JOIN layout
               ON layout.LayoutID = stat.LayoutID
-              INNER JOIN `widget`
+              LEFT OUTER JOIN `widget`
               ON `widget`.widgetId = stat.MediaID
               LEFT OUTER JOIN `widgetoption`
               ON `widgetoption`.widgetId = `widget`.widgetId
-                AND `widgetoption`.type = \'attribute\'
+                AND `widgetoption`.type = \'attrib\'
                 AND `widgetoption`.option = \'name\'
            WHERE stat.type <> \'displaydown\'
                 AND stat.end > :fromDt
@@ -204,11 +204,13 @@ class Stats extends Base
         }
 
         $sql .= '
-            GROUP BY stat.type, display.Display, layout.Layout, IFNULL(widgetoption.value, widget.type)
-            ORDER BY stat.type, display.Display, layout.Layout, IFNULL(widgetoption.value, widget.type)
+            GROUP BY stat.type, display.Display, layout.Layout, CONCAT(widget.type, \'-\', widget.widgetId)
+            ORDER BY stat.type, display.Display, layout.Layout, Name
         ';
 
         $rows = array();
+
+        Log::sql($sql, $params);
 
         foreach (PDOConnect::select($sql, $params) as $row) {
             $entry = [];
@@ -230,8 +232,8 @@ class Stats extends Base
 
     public function availabilityData()
     {
-        $fromDt = Sanitize::getDate('fromDt');
-        $toDt = Sanitize::getDate('toDt');
+        $fromDt = Sanitize::getDate('fromDt', Sanitize::getDate('availabilityFromDt'));
+        $toDt = Sanitize::getDate('toDt', Sanitize::getDate('availabilityToDt'));
         $displayId = Sanitize::getInt('displayId');
 
         // Get an array of display id this user has access to.
