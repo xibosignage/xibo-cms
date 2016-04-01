c88450311d9bd56999dcece1fd0c71c3eac9b889
Dan Garner <dan@springsignage.com>
2016-04-01 10:02:01 +0100

    Increase the size of the list content field on dataset column xibosignage/xibo#758

--- install/database/92.sql
+++ install/database/92.sql
@@ -2,6 +2,8 @@
 INSERT INTO `setting` (`setting`, `value`, `fieldType`, `helptext`, `options`, `cat`, `userChange`, `title`, `validation`, `ordering`, `default`, `userSee`, `type`) VALUES
 ('CDN_URL', '', 'text', 'Content Delivery Network Address for serving file requests to Players', '', 'network', 0, 'CDN Address', '', 33, '', 0, 'text');
 
+ALTER TABLE  `datasetcolumn` CHANGE  `ListContent`  `ListContent` VARCHAR( 1000 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
 UPDATE `version` SET `app_ver` = '1.7.7', `XmdsVersion` = 4, `XlfVersion` = 2;
 UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
 UPDATE `version` SET `DBVersion` = '92';
