c3180a1f751fa564b1a784a9b95262bbc5cd6505
Dan Garner <dan@springsignage.com>
2016-04-01 13:33:00 +0100

    Add missing hardware acceleration webview setting

--- lib/Entity/DisplayProfile.php
+++ lib/Entity/DisplayProfile.php
@@ -1063,6 +1063,22 @@ class DisplayProfile
                         'helpText' => __('The standard browser cache will be used - we recommend this is switched off unless specifically required. Effects Web Page and Embedded.'),
                         'enabled' => true,
                         'groupClass' => NULL
+                    ),
+                    array(
+                        'name' => 'hardwareAccelerateWebViewMode',
+                        'tabId' => 'advanced',
+                        'title' => __('Hardware Accelerate Web Content?'),
+                        'type' => 'string',
+                        'fieldType' => 'dropdown',
+                        'options' => array(
+                            array('id' => '0', 'value' => __('Off')),
+                            array('id' => '2', 'value' => __('Off when transparent')),
+                            array('id' => '1', 'value' => __('On'))
+                        ),
+                        'default' => '1',
+                        'helpText' => __('Mode for hardware acceleration of web based content.'),
+                        'enabled' => true,
+                        'groupClass' => NULL
                     )
                 )
             )
