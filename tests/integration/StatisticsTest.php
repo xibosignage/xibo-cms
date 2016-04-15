3c7bf96dfc00ba836a6ca11fdf9b29a6403d88ad
Peter <peter@springsignage.com>
2016-04-15 09:37:50 +0100

    Statistics- default and custom values

---
+++ tests/integration/StatisticsTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Spring Signage Ltd - http://www.springsignage.com
+ * Copyright (C) 2015 Spring Signage Ltd
+ * (StatisticsTest.php)
+ */
+
+namespace Xibo\Tests\Integration;
+
+use Xibo\Tests\LocalWebTestCase;
+
+class StatisticsTest extends LocalWebTestCase
+{
+
+// Default values
+
+    public function testListAll()
+    {
+        $this->client->get('/stats');
+
+        $this->assertSame(200, $this->client->response->status());
+        $this->assertNotEmpty($this->client->response->body());
+
+        $object = json_decode($this->client->response->body());
+    //    fwrite(STDERR, $this->client->response->body());
+
+        $this->assertObjectHasAttribute('data', $object, $this->client->response->body());
+    }
+
+
+// Custom values
+
+        public function testListAll2()
+    {
+        $this->client->get('/stats' , [
+        	'fromDt' => '2016-04-15 09:00:00',
+        	'toDt' => '2016-04-15 10:00:00',
+        	'displayId' => 2,
+        	'mediaId' => [16]
+        	]);
+
+        $this->assertSame(200, $this->client->response->status());
+        $this->assertNotEmpty($this->client->response->body());
+
+        $object = json_decode($this->client->response->body());
+   //     fwrite(STDERR, $this->client->response->body());
+
+        $this->assertObjectHasAttribute('data', $object, $this->client->response->body());
+    }
+}
