a021d2b73adfaa0cc8b3d894a80bbd10faf9089a
Dan Garner <dan@springsignage.com>
2016-09-21 16:12:52 +0100

    Fix text reflow when resizing a region xibosignage/xibo#843

--- modules/preview/xibo-text-render.js
+++ modules/preview/xibo-text-render.js
@@ -36,6 +36,22 @@ jQuery.fn.extend({
 
         options = $.extend({}, defaults, options);
 
+        // Calculate the dimensions of this item based on the preview/original dimensions
+        var width, height = 0;
+        if (options.previewWidth === 0 || options.previewHeight === 0) {
+            width = options.originalWidth;
+            height = options.originalHeight;
+        }
+        else {
+            width = options.previewWidth;
+            height = options.previewHeight;
+        }
+
+        if (options.scaleOverride !== 0) {
+            width = width / options.scaleOverride;
+            height = height / options.scaleOverride;
+        }
+
         // For each matched element
         this.each(function() {
 
@@ -147,8 +163,8 @@ jQuery.fn.extend({
 
                 // Set the width on the cycled slides
                 $(slides, this).css({
-                    width: options.originalWidth,
-                    height: options.originalHeight
+                    width: width,
+                    height: height
                 });
 
                 // Cycle handles this for us
@@ -191,8 +207,8 @@ jQuery.fn.extend({
                         scaleFactor: options.scaleFactor,
                         behaviour: "scroll",
                         direction: options.direction,
-                        height: options.originalHeight,
-                        width: options.originalWidth
+                        height: height,
+                        width: width
                     });
 
                 $(this).wrapInner(scroller);
