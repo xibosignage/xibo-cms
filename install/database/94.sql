62df21be42c984fd1da1c853fba331772132ddfc
Dan Garner <dan@springsignage.com>
2016-09-27 08:05:12 +0100

    Fix incorrect db table/column in update

--- install/database/94.sql
+++ install/database/94.sql
@@ -1,5 +1,5 @@
 
-UPDATE `modules` SET `name` = 'Weather', `description` = 'Weather Powered by DarkSky' WHERE `type` = 'forecastio';
+UPDATE `module` SET `name` = 'Weather', `description` = 'Weather Powered by DarkSky' WHERE `Module` = 'forecastio';
 
 UPDATE `version` SET `app_ver` = '1.7.9', `XmdsVersion` = 4, `XlfVersion` = 2;
 UPDATE `setting` SET `value` = 0 WHERE `setting` = 'PHONE_HOME_DATE';
