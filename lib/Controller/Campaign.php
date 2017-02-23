22722ea9de248dd05ca64015207eea385c59ff62
Dan Garner <dan@springsignage.com>
2017-02-23 17:50:44 +0000

    I have half fixed issue 941 by improving the backend logic for unassigning a layout with a display order. I believe now that the front end isn't tracking assignments/unassignments correctly. xibosignage/xibo#1047

--- lib/Controller/Campaign.php
+++ lib/Controller/Campaign.php
@@ -24,8 +24,8 @@ use Xibo\Entity\Permission;
 use Xibo\Exception\AccessDeniedException;
 use Xibo\Factory\CampaignFactory;
 use Xibo\Factory\LayoutFactory;
-use Xibo\Factory\TagFactory;
 use Xibo\Factory\PermissionFactory;
+use Xibo\Factory\TagFactory;
 use Xibo\Factory\UserGroupFactory;
 use Xibo\Service\ConfigServiceInterface;
 use Xibo\Service\DateServiceInterface;
@@ -183,6 +183,9 @@ class Campaign extends Base
 
             // Buttons based on permissions
             if ($this->getUser()->checkEditable($campaign)) {
+
+                $campaign->buttons[] = ['divider' => true];
+
                 // Assign Layouts
                 $campaign->buttons[] = array(
                     'id' => 'campaign_button_layouts',
@@ -190,12 +193,16 @@ class Campaign extends Base
                     'text' => __('Layouts')
                 );
 
+                $campaign->buttons[] = ['divider' => true];
+
                 // Edit the Campaign
                 $campaign->buttons[] = array(
                     'id' => 'campaign_button_edit',
                     'url' => $this->urlFor('campaign.edit.form', ['id' => $campaign->campaignId]),
                     'text' => __('Edit')
                 );
+            } else {
+                $campaign->buttons[] = ['divider' => true];
             }
 
             if ($this->getUser()->checkDeleteable($campaign)) {
@@ -208,6 +215,9 @@ class Campaign extends Base
             }
 
             if ($this->getUser()->checkPermissionsModifyable($campaign)) {
+
+                $campaign->buttons[] = ['divider' => true];
+
                 // Permissions for Campaign
                 $campaign->buttons[] = array(
                     'id' => 'campaign_button_delete',
