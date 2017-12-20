ccbfa873ba97e7269d7e9334f77144df97e44a07
Dan Garner <dan@springsignage.com>
2017-12-20 15:32:17 +0000

    Apply 1.7 fix for dependents as nodes in v4 xibosignage/xibo#1343

--- lib/Xmds/Soap.php
+++ lib/Xmds/Soap.php
@@ -830,40 +830,37 @@ class Soap
             // If our dependents are nodes, then build a list of layouts we can use to query for nodes
             $layoutDependents = [];
 
-            if ($options['dependentsAsNodes']) {
+            // Layouts (pop in the default)
+            $layoutIds = [$this->display->defaultLayoutId];
 
-                // Layouts (pop in the default)
-                $layoutIds = [$this->display->defaultLayoutId];
-
-                foreach ($events as $event) {
-                    if ($event['layoutId'] != null && !in_array($event['layoutId'], $layoutIds))
-                        $layoutIds[] = $event['layoutId'];
-                }
-
-                $SQL = '
-                    SELECT DISTINCT `region`.layoutId, `media`.storedAs
-                      FROM `media`
-                        INNER JOIN `lkwidgetmedia`
-                        ON `lkwidgetmedia`.MediaID = `media`.MediaID
-                        INNER JOIN `widget`
-                        ON `widget`.widgetId = `lkwidgetmedia`.widgetId
-                        INNER JOIN `lkregionplaylist`
-                        ON `lkregionplaylist`.playlistId = `widget`.playlistId
-                        INNER JOIN `region`
-                        ON `region`.regionId = `lkregionplaylist`.regionId
-                     WHERE `region`.layoutId IN (' . implode(',', $layoutIds) . ')
-                      AND media.type <> \'module\'
-                ';
-
-                foreach ($this->getStore()->select($SQL, []) as $row) {
-                    if (!array_key_exists($row['layoutId'], $layoutDependents))
-                        $layoutDependents[$row['layoutId']] = [];
-
-                    $layoutDependents[$row['layoutId']][] = $row['storedAs'];
-                }
-
-                $this->getLog()->debug('Resolved dependents for Schedule: %s.', json_encode($layoutDependents, JSON_PRETTY_PRINT));
+            foreach ($events as $event) {
+                if ($event['layoutId'] != null && !in_array($event['layoutId'], $layoutIds))
+                    $layoutIds[] = $event['layoutId'];
             }
+
+            $SQL = '
+                SELECT DISTINCT `region`.layoutId, `media`.storedAs
+                  FROM `media`
+                    INNER JOIN `lkwidgetmedia`
+                    ON `lkwidgetmedia`.MediaID = `media`.MediaID
+                    INNER JOIN `widget`
+                    ON `widget`.widgetId = `lkwidgetmedia`.widgetId
+                    INNER JOIN `lkregionplaylist`
+                    ON `lkregionplaylist`.playlistId = `widget`.playlistId
+                    INNER JOIN `region`
+                    ON `region`.regionId = `lkregionplaylist`.regionId
+                 WHERE `region`.layoutId IN (' . implode(',', $layoutIds) . ')
+                  AND media.type <> \'module\'
+            ';
+
+            foreach ($this->getStore()->select($SQL, []) as $row) {
+                if (!array_key_exists($row['layoutId'], $layoutDependents))
+                    $layoutDependents[$row['layoutId']] = [];
+
+                $layoutDependents[$row['layoutId']][] = $row['storedAs'];
+            }
+
+            $this->getLog()->debug('Resolved dependents for Schedule: %s.', json_encode($layoutDependents, JSON_PRETTY_PRINT));
 
             $overlayNodes = null;
 
@@ -925,19 +922,23 @@ class Soap
                         $layout->setAttribute("scheduleid", $scheduleId);
                         $layout->setAttribute("priority", $is_priority);
 
-                        if (!$options['dependentsAsNodes']) {
-                            $dependents = $this->getSanitizer()->string($row['Dependents']);
-                            $layout->setAttribute("dependents", $dependents);
-                        } else if (array_key_exists($layoutId, $layoutDependents)) {
-                            $dependentNode = $scheduleXml->createElement("dependents");
+                        // Handle dependents
+                        if (array_key_exists($layoutId, $layoutDependents)) {
+                            if ($options['dependentsAsNodes']) {
+                                // Add the dependents to the layout as new nodes
+                                $dependentNode = $scheduleXml->createElement("dependents");
 
-                            foreach ($layoutDependents[$layoutId] as $storedAs) {
-                                $fileNode = $scheduleXml->createElement("file", $storedAs);
+                                foreach ($layoutDependents[$layoutId] as $storedAs) {
+                                    $fileNode = $scheduleXml->createElement("file", $storedAs);
 
-                                $dependentNode->appendChild($fileNode);
+                                    $dependentNode->appendChild($fileNode);
+                                }
+
+                                $layout->appendChild($dependentNode);
+                            } else {
+                                // Add the dependents to the layout as an attribute
+                                $layout->setAttribute("dependents", implode(',', $layoutDependents[$layoutId]));
                             }
-
-                            $layout->appendChild($dependentNode);
                         }
 
                         $layoutElements->appendChild($layout);
