3b772323a90ce684bed865b499364348ee07e709
Dan Garner <dan@springsignage.com>
2018-02-22 11:08:09 +0000

    Default permissions for DayParts to the Everyone Group xibosignage/xibo#1427

--- db/migrations/20180212143336_daypart_system_entries_as_records.php
+++ db/migrations/20180212143336_daypart_system_entries_as_records.php
@@ -63,5 +63,15 @@ class DaypartSystemEntriesAsRecords extends AbstractMigration
         // Execute some SQL to bring the existing records into line.
         $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isCustom = 1) WHERE dayPartId = 0');
         $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isAlways = 1) WHERE dayPartId = 1');
+
+        // Add some default permissions
+        $this->execute('
+            INSERT INTO `permission` (entityId, groupId, objectId, view, edit, `delete`)
+              SELECT entityId, groupId, dayPartId, 1, 0, 0
+               FROM daypart
+                CROSS JOIN permissionentity
+                CROSS JOIN `group`
+              WHERE entity LIKE \'%DayPart\' AND IsEveryone = 1 AND (isCustom = 1 OR isAlways = 1);
+        ');
     }
 }
