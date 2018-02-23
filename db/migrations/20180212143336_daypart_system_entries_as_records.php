9d116592a587149cd9c06c78a0da314057b30a3d
Dan Garner <dan@springsignage.com>
2018-02-23 17:23:25 +0000

    Swap order of daypart update on schedule. Also only do it if we need to add the columns. xibosignage/xibo#1427

--- db/migrations/20180212143336_daypart_system_entries_as_records.php
+++ db/migrations/20180212143336_daypart_system_entries_as_records.php
@@ -32,46 +32,49 @@ class DaypartSystemEntriesAsRecords extends AbstractMigration
     public function up()
     {
         $dayPart = $this->table('daypart');
-        $dayPart
-            ->addColumn('isAlways', 'integer', ['default' => 0, 'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
-            ->addColumn('isCustom', 'integer', ['default' => 0, 'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
-            ->insert([
-                [
-                    'name' => 'Custom',
-                    'description' => 'User specifies the from/to date',
-                    'isRetired' => 0,
-                    'userid' => 1,
-                    'startTime' => '',
-                    'endTime' => '',
-                    'exceptions' => '',
-                    'isAlways' => 0,
-                    'isCustom' => 1
-                ],[
-                    'name' => 'Always',
-                    'description' => 'Event runs always',
-                    'isRetired' => 0,
-                    'userid' => 1,
-                    'startTime' => '',
-                    'endTime' => '',
-                    'exceptions' => '',
-                    'isAlways' => 1,
-                    'isCustom' => 0
-                ]
-            ])
-            ->save();
 
-        // Execute some SQL to bring the existing records into line.
-        $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isCustom = 1) WHERE dayPartId = 0');
-        $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isAlways = 1) WHERE dayPartId = 1');
+        if (!$dayPart->hasColumn('isAlways')) {
+            $dayPart
+                ->addColumn('isAlways', 'integer', ['default' => 0, 'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
+                ->addColumn('isCustom', 'integer', ['default' => 0, 'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
+                ->insert([
+                    [
+                        'name' => 'Custom',
+                        'description' => 'User specifies the from/to date',
+                        'isRetired' => 0,
+                        'userid' => 1,
+                        'startTime' => '',
+                        'endTime' => '',
+                        'exceptions' => '',
+                        'isAlways' => 0,
+                        'isCustom' => 1
+                    ], [
+                        'name' => 'Always',
+                        'description' => 'Event runs always',
+                        'isRetired' => 0,
+                        'userid' => 1,
+                        'startTime' => '',
+                        'endTime' => '',
+                        'exceptions' => '',
+                        'isAlways' => 1,
+                        'isCustom' => 0
+                    ]
+                ])
+                ->save();
 
-        // Add some default permissions
-        $this->execute('
-            INSERT INTO `permission` (entityId, groupId, objectId, view, edit, `delete`)
-              SELECT entityId, groupId, dayPartId, 1, 0, 0
-               FROM daypart
-                CROSS JOIN permissionentity
-                CROSS JOIN `group`
-              WHERE entity LIKE \'%DayPart\' AND IsEveryone = 1 AND (isCustom = 1 OR isAlways = 1);
-        ');
+            // Execute some SQL to bring the existing records into line.
+            $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isAlways = 1) WHERE dayPartId = 1');
+            $this->execute('UPDATE `schedule` SET dayPartId = (SELECT dayPartId FROM daypart WHERE isCustom = 1) WHERE dayPartId = 0');
+
+            // Add some default permissions
+            $this->execute('
+                INSERT INTO `permission` (entityId, groupId, objectId, view, edit, `delete`)
+                  SELECT entityId, groupId, dayPartId, 1, 0, 0
+                   FROM daypart
+                    CROSS JOIN permissionentity
+                    CROSS JOIN `group`
+                  WHERE entity LIKE \'%DayPart\' AND IsEveryone = 1 AND (isCustom = 1 OR isAlways = 1);
+            ');
+        }
     }
 }
