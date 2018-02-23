2de0393f632dab0b2c4b5dc5d8a34f606c40546a
Dan Garner <dan@springsignage.com>
2018-02-23 18:08:17 +0000

    Add a showFilter and showSort option to DataSet Columns so that we can support larger than 33 columns before data tables breaks. xibosignage/xibo#1430

--- db/migrations/20180223180534_data_set_column_filter_and_sort_options_migration.php
+++ db/migrations/20180223180534_data_set_column_filter_and_sort_options_migration.php
@@ -21,14 +21,23 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class MailFromNameSettingMigration extends AbstractMigration
+/**
+ * Class DataSetColumnFilterAndSortOptionsMigration
+ */
+class DataSetColumnFilterAndSortOptionsMigration extends AbstractMigration
 {
-    /** @inheritdoc */
-    public function up()
+    /**
+     * Add the show filter and show sort columns if they do not yet exist.
+     */
+    public function change()
     {
-        // Check to see if the mail_from_name setting exists
-        if (!$this->fetchRow('SELECT * FROM `setting` WHERE setting = \'mail_from_name\'')) {
-            $this->execute('INSERT INTO setting (setting, value, fieldType, helptext, options, cat, userChange, title, validation, ordering, `default`, userSee, type) VALUES (\'mail_from_name\', \'\', \'text\', \'Mail will be sent under this name\', NULL, \'maintenance\', 1, \'Sending email name\', \'\', 45, \'\', 1, \'string\');');
+        $table = $this->table('datasetcolumn');
+
+        if (!$table->hasColumn('showFilter')) {
+            $table
+                ->addColumn('showFilter', 'integer', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY, 'default' => 1])
+                ->addColumn('showSort', 'integer', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY, 'default' => 1])
+                ->update();
         }
     }
 }
