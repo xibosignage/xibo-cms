d72989447064acc25f2a00f5d1557ec2c23854e1
Dan Garner <dan@springsignage.com>
2018-04-17 14:53:03 +0100

    Bump version on cypress test

--- cypress/integration/unauthed_spec.js
+++ cypress/integration/unauthed_spec.js
@@ -4,7 +4,7 @@ describe('Unauthenticated CMS access', function () {
 
         cy.url().should('include', '/login');
 
-        cy.contains('Version 1.8.8');
+        cy.contains('Version 1.8.9');
     });
 
     it('should redirect to login when an authenticated page is requested', function() {
