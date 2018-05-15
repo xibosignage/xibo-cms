1ec8ea98647ed5f788bec021d13a543a04a6a4c5
Dan Garner <dan@springsignage.com>
2018-05-15 11:24:35 +0100

    Add base cypress tests for Playlists

---
+++ cypress/integration/playlists_spec.js
@@ -0,0 +1,31 @@
+describe('Playlists Admin', function () {
+
+    var testRun = Cypress._.random(0, 1e6);
+
+    beforeEach(function () {
+        cy.login();
+    });
+
+    it('should show a list of Playlists', function () {
+        cy.visit('/playlist/view');
+
+        cy.contains('Playlists');
+
+        cy.contains('Showing 1 to');
+    });
+
+    it('should add a non-dynamic playlist', function() {
+        cy.visit('/playlist/view');
+
+        // Click on the Add Playlist button
+        cy.contains('Add Playlist').click();
+
+        cy.get('.modal input#name')
+            .type('Cypress Test Playlist' + testRun);
+
+        cy.get('.modal .save-button').click();
+
+        cy.contains('Cypress Test Playlist');
+    });
+
+});
--- cypress/integration/unauthed_spec.js
+++ cypress/integration/unauthed_spec.js
@@ -4,7 +4,7 @@ describe('Unauthenticated CMS access', function () {
 
         cy.url().should('include', '/login');
 
-        cy.contains('Version 1.8.9');
+        cy.contains('Version 2.0.0-alpha1');
     });
 
     it('should redirect to login when an authenticated page is requested', function() {
