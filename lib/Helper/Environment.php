e1c671e6bbb78af359feda72d6dd6782d9a7513a
Dan Garner <dan@springsignage.com>
2018-05-31 12:16:04 +0100

    Bump version.

--- lib/Helper/Environment.php
+++ lib/Helper/Environment.php
@@ -11,8 +11,8 @@ namespace Xibo\Helper;
 
 class Environment
 {
-    public static $WEBSITE_VERSION_NAME = '1.8.9';
-    public static $WEBSITE_VERSION = 140;
+    public static $WEBSITE_VERSION_NAME = '1.8.10';
+    public static $WEBSITE_VERSION = 141;
 
     public static $VERSION_REQUIRED = '5.5';
     public static $VERSION_UNSUPPORTED = '8.0';
