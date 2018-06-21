caec67dcc55fefc96ac7ff1df42a011ed0bd635b
Dan Garner <dan@springsignage.com>
2018-06-21 15:23:40 +0100

    Test XTR - missing module factory in Regular Maintenance, fix migration for adding wdiget sync.

--- db/migrations/20180621134013_add_widget_sync_task_migration.php
+++ db/migrations/20180621134013_add_widget_sync_task_migration.php
@@ -31,7 +31,7 @@ class AddWidgetSyncTaskMigration extends AbstractMigration
     {
         // Check to see if the mail_from_name setting exists
         if (!$this->fetchRow('SELECT * FROM `task` WHERE name = \'Widget Sync\'')) {
-            $this->execute('INSERT INTO `task` SET `name`=\'Widget Sync\', `class`=\'\\Xibo\\XTR\\WidgetSyncTask\', `status`=2, `isActive`=1, `configFile`=\'/tasks/widget-sync.task\', `options`=\'{}\', `schedule`=\'*/3 * * * *\';');
+            $this->execute('INSERT INTO `task` SET `name`=\'Widget Sync\', `class`=\'\\\\Xibo\\\\XTR\\\\WidgetSyncTask\', `status`=2, `isActive`=1, `configFile`=\'/tasks/widget-sync.task\', `options`=\'{}\', `schedule`=\'*/3 * * * *\';');
         }
     }
 }
