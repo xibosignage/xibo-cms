dd94888ce87fc4b4481e2b628523e8740b486f6e
Dan Garner <dan@springsignage.com>
2018-10-10 14:30:33 +0100

    Make FK updates to lkwidgetmedia tidy corrupt records

--- db/migrations/20180906115712_add_foreign_keys_to_widget_media_migration.php
+++ db/migrations/20180906115712_add_foreign_keys_to_widget_media_migration.php
@@ -33,12 +33,19 @@ class AddForeignKeysToWidgetMediaMigration extends AbstractMigration
         if (!$this->fetchRow('
             SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE constraint_schema=DATABASE()
                 AND `table_name` = \'lkwidgetmedia\' AND referenced_table_name = \'media\';')) {
+
+            $this->execute('DELETE FROM `lkwidgetmedia` WHERE NOT EXISTS (SELECT mediaId FROM `media` WHERE `media`.mediaId = `lkwidgetmedia`.mediaId) ');
+
             // Add the constraint
             $this->execute('ALTER TABLE `lkwidgetmedia` ADD CONSTRAINT `lkwidgetmedia_ibfk_1` FOREIGN KEY (`mediaId`) REFERENCES `media` (`mediaId`);');
         }
+
         if (!$this->fetchRow('
             SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE constraint_schema=DATABASE()
                 AND `table_name` = \'lkwidgetmedia\' AND referenced_table_name = \'widget\';')) {
+
+            $this->execute('DELETE FROM `lkwidgetmedia` WHERE NOT EXISTS (SELECT widgetId FROM `widget` WHERE `widget`.widgetId = `lkwidgetmedia`.widgetId) ');
+
             // Add the constraint
             $this->execute('ALTER TABLE `lkwidgetmedia` ADD CONSTRAINT `lkwidgetmedia_ibfk_2` FOREIGN KEY (`widgetId`) REFERENCES `widget` (`widgetId`);');
         }
