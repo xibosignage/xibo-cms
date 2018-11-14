40f36c23b9b2d7f51ce405fd5f3aa0a0042c66e9
Dan Garner <dan@xibosignage.com>
2018-11-14 18:13:16 +0000

    Fix issue with migration. OSX-285

--- db/migrations/20181113180337_split_ticker_module_migration.php
+++ db/migrations/20181113180337_split_ticker_module_migration.php
@@ -35,13 +35,13 @@ class SplitTickerModuleMigration extends AbstractMigration
             INSERT INTO `module` 
                 (`Module`, `Name`, `Enabled`, `RegionSpecific`, `Description`, `ImageUri`, `SchemaVersion`, `ValidExtensions`, `PreviewEnabled`, `assignable`, `render_as`, `settings`, `viewPath`, `class`, `defaultDuration`) 
             VALUES
-                (\'datasetticker\', \'DataSet Ticker\', 1, 0, \'Ticker with a DataSet providing the items\', \'forms/ticker.gif\', 1, NULL, 1, 1, \'html\', NULL, \'../modules\', \'Xibo\\\\Widget\\\\DataSetTicker\', 10),;        
+                (\'datasetticker\', \'DataSet Ticker\', 1, 1, \'Ticker with a DataSet providing the items\', \'forms/ticker.gif\', 1, NULL, 1, 1, \'html\', NULL, \'../modules\', \'Xibo\\\\Widget\\\\DataSetTicker\', 10);        
         ');
 
         // Find all of the existing tickers which have a dataSet source, and update them to point at the new
         // module `datasetticker`
         $this->execute('
-            UPDATE `widget` SET type = \'datasetticker\' WHERE type = \'ticker\' AND widgetId IN (SELECT DISTINCT widgetId FROM `widgetoption` WHERE option = \'sourceId\' AND value = \'2\')
+            UPDATE `widget` SET type = \'datasetticker\' WHERE type = \'ticker\' AND widgetId IN (SELECT DISTINCT widgetId FROM `widgetoption` WHERE `option` = \'sourceId\' AND `value` = \'2\')
         ');
     }
 }
