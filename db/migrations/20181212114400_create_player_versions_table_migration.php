2f26f933801dcc95b334b068e130f41ee96a90c7
Peter <peter@springsignage.com>
2019-01-21 11:56:30 +0000

    small tweaks to the player upgrade db migrations

--- db/migrations/20181212114400_create_player_versions_table_migration.php
+++ db/migrations/20181212114400_create_player_versions_table_migration.php
@@ -26,6 +26,8 @@ use Phinx\Migration\AbstractMigration;
 /**
  * Class CreatePlayerVersionsTableMigration
  * Create a new table to store information about player versions
+ * Install playersoftware widget
+ * Remove apk,ipk from validExtensions in genericfiles module
  */
 class CreatePlayerVersionsTableMigration extends AbstractMigration
 {
@@ -39,5 +41,28 @@ class CreatePlayerVersionsTableMigration extends AbstractMigration
             ->addColumn('mediaId', 'integer')
             ->addForeignKey('mediaId', 'media', 'mediaId')
             ->create();
+
+        // Add the player_software module
+        $modules = $this->table('module');
+        if (!$this->fetchRow('SELECT * FROM module WHERE module = \'playersoftware\'')) {
+            $modules->insert([
+                'module' => 'playersoftware',
+                'name' => 'Player Software',
+                'enabled' => 1,
+                'regionSpecific' => 0,
+                'description' => 'A module for managing Player Versions',
+                'schemaVersion' => 1,
+                'validExtensions' => 'apk,ipk,wgt',
+                'previewEnabled' => 0,
+                'assignable' => 0,
+                'renderAs' => null,
+                'viewPath' => '../modules',
+                'class' => 'Xibo\\\Widget\\\PlayerSoftware',
+                'defaultDuration' => 10
+            ])->save();
+        }
+
+        // remove apk and ipk from valid extensions in generic file module
+        $this->execute('UPDATE `module` SET validextensions = REPLACE(validextensions, \'apk,ipk\', \'\') WHERE module = \'genericfile\' LIMIT 1;');
     }
 }
