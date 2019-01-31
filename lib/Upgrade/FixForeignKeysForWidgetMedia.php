fcb7f78b447df43b6c47b68a50f6282622e83a55
Dan Garner <dan@xibosignage.com>
2019-01-31 07:46:12 +0000

    Patch to ensure we remove orphaned lkwidgetmedia records before trying to add the foreign key. (on the widget side)

--- lib/Upgrade/FixForeignKeysForWidgetMedia.php
+++ lib/Upgrade/FixForeignKeysForWidgetMedia.php
@@ -61,7 +61,7 @@ class FixForeignKeysForWidgetMedia implements Step
         if (!$this->store->exists('
             SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE constraint_schema=DATABASE()
                 AND `table_name` = \'lkwidgetmedia\' AND referenced_table_name = \'media\';', [])) {
-            // Delete an records that might be conflicting
+            // Delete any records that might be conflicting
             $this->store->update('DELETE FROM `lkwidgetmedia` WHERE NOT EXISTS (SELECT * FROM `media` WHERE `media`.mediaId = `lkwidgetmedia`.mediaId)', []);
 
             // Add the constraint
@@ -71,6 +71,8 @@ class FixForeignKeysForWidgetMedia implements Step
         if (!$this->store->exists('
             SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE constraint_schema=DATABASE()
                 AND `table_name` = \'lkwidgetmedia\' AND referenced_table_name = \'widget\';', [])) {
+            // Delete any records that might be conflicting
+            $this->store->update('DELETE FROM `lkwidgetmedia` WHERE NOT EXISTS (SELECT * FROM `widget` WHERE `widget`.widgetId = `lkwidgetmedia`.widgetId)', []);
 
             // Add the constraint
             $this->store->update('ALTER TABLE `lkwidgetmedia` ADD CONSTRAINT `lkwidgetmedia_ibfk_2` FOREIGN KEY (`widgetId`) REFERENCES `widget` (`widgetId`);', []);
