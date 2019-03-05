5a51714f07ff1624fdbdb1dd529479158c26ed7b
Dan Garner <dan@xibosignage.com>
2019-03-05 15:25:32 +0000

    Remove version string from upgrade in progress page. Refactor Environment::migrationPending to cache.

--- lib/Helper/Environment.php
+++ lib/Helper/Environment.php
@@ -1,11 +1,24 @@
 <?php
-/*
- * Spring Signage Ltd - http://www.springsignage.com
- * Copyright (C) 2018 Spring Signage Ltd
- * (Environment.php)
+/**
+ * Copyright (C) 2019 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
 namespace Xibo\Helper;
 use Phinx\Console\PhinxApplication;
 use Phinx\Wrapper\TextWrapper;
@@ -22,17 +35,35 @@ class Environment
     public static $VERSION_REQUIRED = '5.5';
     public static $VERSION_UNSUPPORTED = '8.0';
 
+    /** @var null cache migration status for the whole request */
+    private static $_migration_status = null;
+
     /**
      * Is there a migration pending
+     * this function only returns true if we are in DEV mode, otherwise it assumes migrations have been run by an
+     * outside process.
      * @return bool
      */
     public static function migrationPending()
     {
-        // Use a Phinx text wrapper to work out what the current status is
-        $phinx = new TextWrapper(new PhinxApplication(), ['configuration' => PROJECT_ROOT . '/phinx.php']);
-        $phinx->getStatus();
+        return (self::getMigrationStatus() != 0);
+    }
 
-        return ($phinx->getExitCode() != 0);
+    /**
+     * Get Migration Status
+     * @return int
+     */
+    private static function getMigrationStatus()
+    {
+        if (self::$_migration_status === null) {
+            // Use a Phinx text wrapper to work out what the current status is
+            $phinx = new TextWrapper(new PhinxApplication(), ['configuration' => PROJECT_ROOT . '/phinx.php']);
+            $phinx->getStatus();
+
+            self::$_migration_status = $phinx->getExitCode();
+        }
+
+        return self::$_migration_status;
     }
 
     /**
