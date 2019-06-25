fc7c28fea9301f5739e86cd1920ef6c9bbab11c1
Peter <peter@springsignage.com>
2019-06-25 12:08:27 +0100

    OSX-596 Better migration for the move tidyStats to statsArchive task

--- db/migrations/20190620112611_move_tidy_stats_to_stats_archive_task_migration.php
+++ db/migrations/20190620112611_move_tidy_stats_to_stats_archive_task_migration.php
@@ -27,9 +27,28 @@ class MoveTidyStatsToStatsArchiveTaskMigration extends AbstractMigration
     /** @inheritdoc */
     public function change()
     {
-        if (!$this->fetchRow('SELECT * FROM `task` WHERE `name` = \'Stats Archive\' AND `isActive` = 1 ')) {
-            $this->execute('UPDATE `task` SET isActive = 1 WHERE name = \'Stats Archive\' ');
-        }
+        // query the database and look for Stats Archive task
+        $statsArchiveQuery = $this->query('SELECT taskId, name, options, isActive FROM `task` WHERE `name` = \'Stats Archive\' ;');
+        $statsArchiveData = $statsArchiveQuery->fetchAll(PDO::FETCH_ASSOC);
 
+        if (count($statsArchiveData) > 0) {
+            foreach ($statsArchiveData as $row) {
+                $taskId = $row['taskId'];
+                $isActive = $row['isActive'];
+                $options = json_decode($row['options']);
+
+                // if the task is current set as Active, we need to ensure that archiveStats option is set to On (default is Off)
+                if ($isActive == 1) {
+                    $options->archiveStats = 'On';
+                } else {
+                    $options->archiveStats = 'Off';
+                }
+
+                // save updated options to variable
+                $newOptions = json_encode($options);
+
+                $this->execute('UPDATE `task` SET isActive = 1, options = \'' . $newOptions . '\' WHERE taskId = '. $taskId );
+            }
+        }
     }
 }
