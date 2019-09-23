0be7d839a5ce408bbf97633f97b276a926d2e10f
Peter <peter@springsignage.com>
2019-09-23 12:30:40 +0100

    OSX-812 Better handle Widget expiry dates for CMS Instances in different timezones xibosignage/xibo#1934

--- lib/Factory/LayoutFactory.php
+++ lib/Factory/LayoutFactory.php
@@ -438,8 +438,9 @@ class LayoutFactory extends BaseFactory
                 $widget->toDt = ($mediaNode->getAttribute('toDt') === '') ? Widget::$DATE_MAX : $mediaNode->getAttribute('toDt');
 
                 // convert the date string to a unix timestamp, if the layout xlf does not contain dates, then set it to the $DATE_MIN / $DATE_MAX which are already unix timestamps, don't attempt to convert them
-                $widget->fromDt = ($widget->fromDt === Widget::$DATE_MIN) ? Widget::$DATE_MIN : $this->getDate()->parse($widget->fromDt)->format('U');
-                $widget->toDt = ($widget->toDt === Widget::$DATE_MAX) ? Widget::$DATE_MAX : $this->getDate()->parse($widget->toDt)->format('U');
+                // we need to check if provided from and to dates are within $DATE_MIN +- year to avoid issues with CMS Instances in different timezones https://github.com/xibosignage/xibo/issues/1934
+                $widget->fromDt = ($widget->fromDt === Widget::$DATE_MIN || ( $this->getDate()->parse($widget->fromDt)->format('U') > $this->getDate()->parse($this->getDate()->getLocalDate(Widget::$DATE_MIN))->subYear()->format('U') && $this->getDate()->parse($widget->fromDt)->format('U') < $this->getDate()->parse($this->getDate()->getLocalDate(Widget::$DATE_MIN))->addYear()->format('U')) ) ? Widget::$DATE_MIN : $this->getDate()->parse($widget->fromDt)->format('U');
+                $widget->toDt = ($widget->toDt === Widget::$DATE_MAX || ( $this->getDate()->parse($widget->toDt)->format('U') > $this->getDate()->parse($this->getDate()->getLocalDate(Widget::$DATE_MAX))->subYear()->format('U') && $this->getDate()->parse($widget->toDt)->format('U') < $this->getDate()->parse($this->getDate()->getLocalDate(Widget::$DATE_MAX))->addYear()->format('U')) ) ? Widget::$DATE_MAX : $this->getDate()->parse($widget->toDt)->format('U');
 
                 $this->getLog()->debug('Adding Widget to object model. ' . $widget);
 
