c7f41b2285b422b7c4f6fa6e26761994ebac679c
ifarzana <m2farzana@gmail.com>
2019-09-24 13:36:48 +0100

    Proof of play - send report as email

--- lib/Report/ProofOfPlay.php
+++ lib/Report/ProofOfPlay.php
@@ -110,6 +110,18 @@ class ProofOfPlay implements ReportInterface
     }
 
     /** @inheritdoc */
+    public function getReportChartScript($results)
+    {
+        return null;
+    }
+
+    /** @inheritdoc */
+    public function getReportEmailTemplate()
+    {
+        return 'proofofplay-email-template.twig';
+    }
+
+    /** @inheritdoc */
     public function getReportForm()
     {
         return [
@@ -189,6 +201,8 @@ class ProofOfPlay implements ReportInterface
             $schedule = ReportSchedule::$SCHEDULE_YEARLY;
             $filterCriteria['reportFilter'] = 'lastyear';
         }
+
+        $filterCriteria['sendEmail'] = $this->getSanitizer()->getCheckbox('sendEmail');
 
         // Return
         return [
