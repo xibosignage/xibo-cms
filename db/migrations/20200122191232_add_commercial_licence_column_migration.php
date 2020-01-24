3ee6581074aaeb5245de9954cd43c1ebc20b0c45
Peter <peter@springsignage.com>
2020-01-24 11:20:58 +0000

    Display Commercial licence migration xibosignage/xibo#1981

---
+++ db/migrations/20200122191232_add_commercial_licence_column_migration.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2020 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Phinx\Migration\AbstractMigration;
+
+class AddCommercialLicenceColumnMigration extends AbstractMigration
+{
+    /** @inheritdoc */
+    public function change()
+    {
+        $displayTable = $this->table('display');
+
+        // Add new column to Display table - commercialLicence. 0 - Not licensed, 1 - licensed, 2 - trial licence, 3 - not applicable
+        if (!$displayTable->hasColumn('commercialLicence')) {
+            $displayTable
+                ->addColumn('commercialLicence', 'integer', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY, 'default' => null, 'null' => true])
+                ->save();
+        }
+    }
+}
