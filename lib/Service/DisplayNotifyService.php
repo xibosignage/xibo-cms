4c2b04aecf95527a4326d73604f1abffb02e0f28
Peter <peter@springsignage.com>
2020-03-03 12:20:02 +0000

    Support library exceptions and post tests fixes - Middleware, Services, helpers and Storage Interface

--- lib/Service/DisplayNotifyService.php
+++ lib/Service/DisplayNotifyService.php
@@ -1,15 +1,30 @@
 <?php
-/*
- * Spring Signage Ltd - http://www.springsignage.com
- * Copyright (C) 2016 Spring Signage Ltd
- * (DisplayNotifyService.php)
+/**
+ * Copyright (C) 2020 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
 namespace Xibo\Service;
 use Stash\Interfaces\PoolInterface;
 use Xibo\Entity\Display;
-use Xibo\Exception\DeadlockException;
+use Xibo\Support\Exception\DeadlockException;
 use Xibo\Factory\DayPartFactory;
 use Xibo\Factory\ScheduleFactory;
 use Xibo\Storage\StorageServiceInterface;
@@ -118,6 +133,7 @@ class DisplayNotifyService implements DisplayNotifyServiceInterface
 
         // Dump the cache
         foreach ($displayIds as $displayId) {
+            $this->log->debug('dumping cache for ' . $displayId);
             $this->pool->deleteItem(Display::getCachePrefix() . $displayId);
         }
 
