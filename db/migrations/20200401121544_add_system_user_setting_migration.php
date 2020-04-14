3f2d9f8f59097a84a8ef9dd4aa0a0ad92f48c8c2
Peter <peter@xibosignage.com>
2020-04-14 10:36:12 +0100

    Protect System User migration against no super admins in the system

--- db/migrations/20200401121544_add_system_user_setting_migration.php
+++ db/migrations/20200401121544_add_system_user_setting_migration.php
@@ -33,16 +33,19 @@ class AddSystemUserSettingMigration extends AbstractMigration
             $getSystemUserQuery = $this->query('SELECT userId FROM user WHERE userTypeId = 1 ORDER BY userId LIMIT 1');
             $getSystemUserResult = $getSystemUserQuery->fetchAll(PDO::FETCH_ASSOC);
 
-            $userId = $getSystemUserResult[0]['userId'];
+            // if for some reason there are no super admin Users in the CMS, ensure that migration does not fail.
+            if (count($getSystemUserResult) >= 1) {
+                $userId = $getSystemUserResult[0]['userId'];
 
-            $this->table('setting')->insert([
-                [
-                    'setting' => 'SYSTEM_USER',
-                    'value' => $userId,
-                    'userSee' => 1,
-                    'userChange' => 1
-                ]
-            ])->save();
+                $this->table('setting')->insert([
+                    [
+                        'setting' => 'SYSTEM_USER',
+                        'value' => $userId,
+                        'userSee' => 1,
+                        'userChange' => 1
+                    ]
+                ])->save();
+            }
         }
     }
 }
