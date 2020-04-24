ee42e753fe698aea77d61fe73d0559d2778d7b5e
Peter <peter@xibosignage.com>
2020-04-24 14:16:22 +0100

    Change condition for Twig autoReload to isDevMode()

--- lib/Middleware/State.php
+++ lib/Middleware/State.php
@@ -231,8 +231,8 @@ class State implements Middleware
             $filter = new \Twig\TwigFilter('url_decode', 'urldecode');
             $twigEnvironment->addFilter($filter);
 
-            // set Twig auto reload if we are in test mode
-            if (strtolower($mode) == 'test') {
+            // set Twig auto reload if we are in dev mode
+            if (Environment::isDevMode()) {
                 $twigEnvironment->enableAutoReload();
             }
         }
