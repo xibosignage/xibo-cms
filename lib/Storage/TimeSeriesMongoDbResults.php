6da98a8c834730ecbb8b112393c24aaadba240ae
Dan Garner <dan@xibosignage.com>
2020-06-16 17:19:29 +0100

    Stats API campaignId and tags output xibosignage/xibo#2265

--- lib/Storage/TimeSeriesMongoDbResults.php
+++ lib/Storage/TimeSeriesMongoDbResults.php
@@ -76,10 +76,16 @@ class TimeSeriesMongoDbResults implements TimeSeriesResultsInterface
             $entry['count'] = $row['count'];
             $entry['displayId'] = isset($row['displayId']) ? $row['displayId']: 0;
             $entry['layoutId'] = isset($row['layoutId']) ? $row['layoutId']: 0;
+            $entry['campaignId'] = isset($row['campaignId']) ? $row['campaignId']: 0;
             $entry['widgetId'] = isset($row['widgetId']) ? $row['widgetId']: 0;
             $entry['mediaId'] = isset($row['mediaId']) ? $row['mediaId']: 0;
             $entry['statDate'] = isset($row['statDate']) ? $row['statDate']->toDateTime()->format('U') : null;
             $entry['engagements'] = isset($row['engagements']) ? $row['engagements'] : [];
+            $entry['tagFilter'] = isset($row['tagFilter']) ? $row['tagFilter'] : [
+                'dg' => [],
+                'layout' => [],
+                'media' => []
+            ];
 
             $rows[] = $entry;
         }
