cb8109d0af0ee5f89d053386516f3965902f173e
ifarzana <m2farzana@gmail.com>
2020-07-16 12:19:49 +0100

    Statistics Migration Task  xibosignage/xibo#2290

--- lib/XTR/StatsMigrationTask.php
+++ lib/XTR/StatsMigrationTask.php
@@ -376,6 +376,9 @@ class StatsMigrationTask implements TaskInterface
             $statCount = 0;
             foreach ($stats->fetchAll() as $stat) {
 
+                // We get the watermark now because if we skip the records our watermark will never reach 0
+                $watermark = $stat['statId'];
+
                 // Get display
                 $display = $this->getDisplay((int) $stat['displayId']);
 
@@ -400,8 +403,6 @@ class StatsMigrationTask implements TaskInterface
                 $entry['widgetId'] = (int) $stat['widgetId'];
                 $entry['duration'] = (int) $stat['duration'];
                 $entry['count'] = (int) $stat['count'];
-
-                $watermark = $stat['statId'];
 
                 // Add stats in store $this->stats
                 $this->timeSeriesStore->addStat($entry);
@@ -481,6 +482,9 @@ class StatsMigrationTask implements TaskInterface
             $statCount = 0;
             foreach ($stats->fetchAll() as $stat) {
 
+                // We get the watermark now because if we skip the records our watermark will never reach 0
+                $watermark = $stat['statId'];
+
                 // Get display
                 $display = $this->getDisplay((int) $stat['displayId']);
 
@@ -489,7 +493,6 @@ class StatsMigrationTask implements TaskInterface
                     continue;
                 }
 
-                $watermark = $stat['statId'];
                 $entry = [];
 
                 // Get campaignId
