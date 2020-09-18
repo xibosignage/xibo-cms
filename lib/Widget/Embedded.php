aa8f64197ec70f58e97c41a0a641a4bbd9a56dba
Dan Garner <dan@xibosignage.com>
2020-09-18 08:27:30 +0100

    Missing docs xibosignage/xibo#2251

--- lib/Widget/Embedded.php
+++ lib/Widget/Embedded.php
@@ -108,6 +108,13 @@ class Embedded extends ModuleWidget
      *      required=false
      *   ),
      *  @SWG\Parameter(
+     *      name="isPreNavigate",
+     *      in="formData",
+     *      description="Flag (0,1) - Should this Widget be loaded off screen so that it is made ready in the background? Dynamic content will run.",
+     *      type="integer",
+     *      required=false
+     *   ),
+     *  @SWG\Parameter(
      *      name="embedHtml",
      *      in="formData",
      *      description="HTML to embed",
