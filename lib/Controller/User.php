a4ba9fcd644139b5dd3513baf262afd3cd4277fd
Dan Garner <dan@xibosignage.com>
2020-10-22 18:38:34 +0100

    Tweaks to the Feature feature after first round of feedback. xibosignage/xibo#2333

--- lib/Controller/User.php
+++ lib/Controller/User.php
@@ -383,6 +383,9 @@ class User extends Base
 
             $user->includeProperty('buttons');
 
+            // Deal with the home page
+            $user->homePage = $this->userGroupFactory->getHomepageByName($user->homePageId)->title;
+
             // Super admins have some buttons
             if ($this->getUser()->featureEnabled('users.modify')
                 && $this->getUser()->checkEditable($user)
