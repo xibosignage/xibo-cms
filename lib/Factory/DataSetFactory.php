ddb2a2009d6295321a488bcb426be32cacc25d28
Dan Garner <dan@xibosignage.com>
2020-11-18 13:25:46 +0000

    Extra logging in RemoteDataSetFetch.

--- lib/Factory/DataSetFactory.php
+++ lib/Factory/DataSetFactory.php
@@ -459,9 +459,17 @@ class DataSetFactory extends BaseFactory
                     $this->pool->saveDeferred($cacheControlKey);
                 }
 
-                // TODO: we should probably do some checking to ensure we have JSON back
                 if ($dataSet->sourceId === 1) {
-                    $result->entries[] = json_decode($request->getBody());
+                    // Make sure we have JSON in the response
+                    $body = $request->getBody()->getContents();
+
+                    try {
+                        $json = \GuzzleHttp\json_decode($body);
+                    } catch (\GuzzleHttp\Exception\InvalidArgumentException $invalidArgumentException) {
+                        throw new InvalidArgumentException(__('Unable to get Data for %s because the response was not valid JSON.', $dataSet->dataSet), 'url');
+                    }
+
+                    $result->entries[] = $json;
                     foreach ($result->entries as $entry) {
                         $data = $this->getDataRootFromResult($dataSet->dataRoot, $entry);
 
@@ -472,7 +480,7 @@ class DataSetFactory extends BaseFactory
                         }
                     }
                 } else {
-                    $csv = $request->getBody();
+                    $csv = $request->getBody()->getContents();
                     $array = array_map("str_getcsv", explode("\n", $csv));
 
                     if ($dataSet->ignoreFirstRow == 1) {
@@ -627,6 +635,8 @@ class DataSetFactory extends BaseFactory
      * @return array|\stdClass The Data hold in the configured dataRoot
      */
     private function getDataRootFromResult($dataRoot, $result) {
+        $this->getLog()->debug('Getting ' . $dataRoot . 'from result.');
+
         if (empty($dataRoot)) {
             return $result;
         }
