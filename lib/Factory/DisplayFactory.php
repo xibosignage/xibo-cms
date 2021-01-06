02840100ddb81c6f49d00866662716939af8c063
Peter <peter@xibosignage.com>
2021-01-06 16:24:18 +0000

    Fix sorting by Members on Display Group Members form fixes xibosignage/xibo#2389

--- lib/Factory/DisplayFactory.php
+++ lib/Factory/DisplayFactory.php
@@ -468,10 +468,10 @@ class DisplayFactory extends BaseFactory
         }
 
         // run the special query to help sort by displays already assigned to this display group, we want to run it only if we're sorting by member column.
-        if ($parsedBody->getInt('displayGroupIdMembers') !== null && ($sortOrder == ['`member`'] || $sortOrder == ['`member` DESC'] )) {
+        if ($parsedBody->getInt('displayGroupIdMembers') !== null && ($sortOrder == ["`member`"] || $sortOrder == ["`member` DESC"] )) {
             $members = [];
             foreach ($this->getStore()->select($select . $body, $params) as $row) {
-                $displayId = $parsedBody->getInt($row['displayId']);
+                $displayId = $this->getSanitizer($row)->getInt('displayId');
                 $displayGroupId = $parsedBody->getInt('displayGroupIdMembers');
 
                 if ($this->getStore()->exists('SELECT display.display, display.displayId, displaygroup.displayGroupId
