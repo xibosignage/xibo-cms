cd8ead03cc9deadeb7dbb3274f19adafed1d51ae
Dan Garner <dan@xibosignage.com>
2021-02-25 12:12:08 +0000

    Increase the size on the group.features column. xibosignage/xibo#2420

--- db/migrations/20201007093511_features_migration.php
+++ db/migrations/20201007093511_features_migration.php
@@ -33,7 +33,7 @@ class FeaturesMigration extends AbstractMigration
     public function change()
     {
         $this->table('group')
-            ->addColumn('features', 'string', [
+            ->addColumn('features', 'text', [
                 'null' => true,
                 'default' => null,
                 'limit' => \Phinx\Db\Adapter\MysqlAdapter::TEXT_LONG
