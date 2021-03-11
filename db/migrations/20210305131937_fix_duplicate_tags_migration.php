3d6ef9f64f90a4152b9b056f446869fb2d1314e4
Peter <peter@xibosignage.com>
2021-03-11 10:53:27 +0000

    Fix duplicate tags migration fixes xibosignage/xibo#2444

--- db/migrations/20210305131937_fix_duplicate_tags_migration.php
+++ db/migrations/20210305131937_fix_duplicate_tags_migration.php
@@ -14,10 +14,29 @@ class FixDuplicateTagsMigration extends AbstractMigration
     /** @inheritDoc */
     public function change()
     {
-        // remove empty/odd tags
-        $this->execute('DELETE FROM `tag` WHERE `tag`.tag = \'\' OR `tag`.tag = \',\' OR `tag`.tag = \' \' ; ');
+        // Get the duplicate tags leaving lowest ids and any empty/odd tags
+        $tagsToRemove = $this->query('SELECT DISTINCT t1.tagId FROM `tag` t1 INNER JOIN `tag` t2 WHERE (t1.tagId > t2.tagId AND t1.tag = t2.tag) OR (t1.tag = \'\' OR t1.tag = \',\' OR t1.tag = \' \') ');
+        $tagsToRemoveData = $tagsToRemove->fetchAll(PDO::FETCH_ASSOC);
 
-        // remove duplicates, keeping lowest tagId
-        $this->execute('DELETE t1 FROM `tag` t1 INNER JOIN `tag` t2 WHERE t1.tagId > t2.tagId AND t1.tag = t2.tag; ');
+        // only execute this code if any tags we need to delete were found
+        if (count($tagsToRemoveData) > 0) {
+            $tagIds = [];
+            foreach ($tagsToRemoveData as $row) {
+                $tagIds[] = $row['tagId'];
+            }
+
+            // convert the array to comma separated string which we can use in a sql statement
+            $tagIds = "'" .implode("','", $tagIds  ) . "'";
+
+            // remove links to the tags we want to remove from lktag tables
+            $this->execute("DELETE FROM `lktagcampaign` WHERE tagId IN ($tagIds)");
+            $this->execute("DELETE FROM `lktagdisplaygroup` WHERE tagId IN ($tagIds)");
+            $this->execute("DELETE FROM `lktaglayout` WHERE tagId IN ($tagIds)");
+            $this->execute("DELETE FROM `lktagmedia` WHERE tagId IN ($tagIds)");
+            $this->execute("DELETE FROM `lktagplaylist` WHERE tagId IN ($tagIds)");
+
+            // finally remove the tag itself from tag table
+            $this->execute("DELETE FROM `tag` WHERE tagId IN ($tagIds)");
+        }
     }
 }
