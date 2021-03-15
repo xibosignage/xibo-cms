e3d37a4e4a4ec9a14724ca674d26b756d33ee526
maurofmferrao <maurofmferrao@gmail.com>
2021-03-15 09:57:01 +0000

    Preview layout: Preview sometimes continues to play for longer than expected. fixes  xibosignage/xibo#2436

--- lib/Widget/Countdown.php
+++ lib/Widget/Countdown.php
@@ -76,7 +76,7 @@ class Countdown extends ModuleWidget
             $module->regionSpecific = 1;
             $module->renderAs = 'html';
             $module->schemaVersion = $this->codeSchemaVersion;
-            $module->defaultDuration = 5;
+            $module->defaultDuration = 60;
             $module->settings = [];
             $module->installName = 'countdown';
 
