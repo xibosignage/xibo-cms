f1214fd39d7ec51891fd855a0fa888286b228727
maurofmferrao <maurofmferrao@gmail.com>
2021-05-24 13:09:00 +0100

    Xibo Layout Scaler wrong dimensions on Android fixes  xibosignage/xibo#2516

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['google'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+  },
+};
