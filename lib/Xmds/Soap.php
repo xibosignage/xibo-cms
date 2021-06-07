71e20fbca430779b21b0596fbf83a0324ab644c3
Dan Garner <dan@xibosignage.com>
2021-06-07 14:23:32 +0100

    Actions: navLayout too restrictive in getActionLayoutIds. xibosignage/xibo#2490

--- lib/Xmds/Soap.php
+++ lib/Xmds/Soap.php
@@ -441,7 +441,7 @@ class Soap
         // workout if any of the layouts we have in our list has Actions pointing to another Layout.
         foreach ($layouts as $layoutId) {
             $layout = $this->layoutFactory->loadById($layoutId);
-            $actionLayoutIds = $layout->getActionLayoutIds();
+            $actionLayoutIds = $layout->getActionPublishedLayoutIds();
 
             // merge the Action layouts to our array, we need the player to download all resources on them
             if (!empty($actionLayoutIds) ) {
