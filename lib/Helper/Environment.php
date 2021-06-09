fb5faa4c80054bfe06c547fb467e8456551b5a7f
Peter <peter@xibosignage.com>
2021-06-09 11:57:43 +0100

    XMDS : Increment version to 6 to accomodate blacklist changes coming in 3.1 Remove Blacklist from new wsdl, add Report Faults to new wsdl and Soap

--- lib/Helper/Environment.php
+++ lib/Helper/Environment.php
@@ -30,7 +30,7 @@ use Phinx\Wrapper\TextWrapper;
 class Environment
 {
     public static $WEBSITE_VERSION_NAME = '3.0.0-rc3';
-    public static $XMDS_VERSION = '5';
+    public static $XMDS_VERSION = '6';
     public static $XLF_VERSION = 3;
     public static $VERSION_REQUIRED = '7.2.9';
     public static $VERSION_UNSUPPORTED = '8.0';
