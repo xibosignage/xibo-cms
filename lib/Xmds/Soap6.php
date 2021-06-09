f9701fc3e8c07ad6210429f48bd30e7f738c6f09
Peter <peter@xibosignage.com>
2021-06-09 12:03:20 +0100

    Fix php docs for Soap6

--- lib/Xmds/Soap6.php
+++ lib/Xmds/Soap6.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2020 Xibo Signage Ltd
+ * Copyright (C) 2021 Xibo Signage Ltd
  *
  * Xibo - Digital Signage - http://www.xibo.org.uk
  *
@@ -22,6 +22,10 @@
 
 namespace Xibo\Xmds;
 
+/**
+ * Class Soap6
+ * @package Xibo\Xmds
+ */
 class Soap6 extends Soap5
 {
     /**
