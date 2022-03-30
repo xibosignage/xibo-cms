03a0d1ce2f6835de491b8c0928283f76d16a4986
Dan Garner <dan@xibosignage.com>
2022-03-30 12:46:50 +0100

    WIP: add migration

---
+++ db/migrations/20220330111440_modules_table_ver_four_migration.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * Copyright (C) 2022 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Convert the modules table for v4
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+ */
+class ModulesTableVerFourMigration extends AbstractMigration
+{
+    /** @inheritDoc */
+    public function change()
+    {
+        // Rename the old table.
+        $this->table('module')->rename('module_old');
+
+        // Add our new table
+        $this->table('module', ['id' => false, 'primary_key' => ['moduleId']])
+            ->addColumn('moduleId', 'string', [
+                'limit' => 35,
+                'null' => false
+            ])
+            ->addColumn('enabled', 'integer', [
+                'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY,
+                'default' => 0
+            ])
+            ->addColumn('previewEnabled', 'integer', [
+                'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY,
+                'default' => 1
+            ])
+            ->addColumn('defaultDuration', 'integer', [
+                'limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY,
+                'default' => 60
+            ])
+            ->addColumn('settings', 'text', [
+                'default' => null,
+                'null' => true
+            ])
+            ->save();
+
+        // Pull through old modules, having a guess at their names.
+        try {
+            $this->execute('
+            INSERT INTO `module` (`moduleId`, `enabled`, `previewEnabled`, `defaultDuration`, `settings`)
+            SELECT DISTINCT LOWER(CASE WHEN `class` LIKE \'%Custom%\' 
+                       THEN IFNULL(installname, module)
+                       ELSE CONCAT(\'core-\', `module`)
+                   END),
+                   `enabled`,
+                   `previewEnabled`,
+                   `defaultDuration`, 
+                   `settings`
+              FROM `module_old`');
+
+            // Drop the old table
+            $this->dropTable('module_old');
+        } catch (Exception $e) {
+            // Keep the old module table around for diagnosis and just continue on.
+        }
+    }
+}
