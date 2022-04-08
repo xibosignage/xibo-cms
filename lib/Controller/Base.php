1d0b34f0cdd908188af5d6f281d9616246780165
Dan Garner <dan@xibosignage.com>
2022-04-08 14:36:19 +0100

    Datatables: improve sanitization on sorting. xibosignage/xibo#2785

--- lib/Controller/Base.php
+++ lib/Controller/Base.php
@@ -389,11 +389,18 @@ class Base
         if ($columns == null || !is_array($columns))
             return null;
 
-        $order = array_map(function ($element) use ($columns) {
-            return ((isset($columns[$element['column']]['name']) && $columns[$element['column']]['name'] != '') ? '`' . $columns[$element['column']]['name'] . '`' : '`' . $columns[$element['column']]['data'] . '`') . (($element['dir'] == 'desc') ? ' DESC' : '');
-        }, $app->request()->get('order', array()));
+        $order = $app->request()->get('order');
+        if ($order === null || !is_array($order) || count($order) <= 0) {
+            return null;
+        }
 
-        return $order;
+        return array_map(function ($element) use ($columns) {
+            $val = (isset($columns[$element['column']]['name']) && $columns[$element['column']]['name'] != '')
+                ? $columns[$element['column']]['name']
+                : $columns[$element['column']]['data'];
+            $val = preg_replace('/[^A-Za-z0-9_]/', '', $val);
+            return '`' . $val . '`' . (($element['dir'] == 'desc') ? ' DESC' : '');
+        }, $order);
     }
 
     /**
