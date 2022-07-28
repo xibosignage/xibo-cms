a0173bf46743711944eebfe86f09777b78eb35f6
ifarzana <m2farzana@gmail.com>
2022-07-28 12:00:14 +0100

    Map shows displays within bound

--- lib/Controller/Display.php
+++ lib/Controller/Display.php
@@ -397,6 +397,7 @@ class Display extends Base
             'commercialLicence' => $parsedQueryParams->getInt('commercialLicence'),
             'folderId' => $parsedQueryParams->getInt('folderId'),
             'logicalOperator' => $parsedQueryParams->getString('logicalOperator'),
+            'bounds' => $parsedQueryParams->getString('bounds'),
         ];
     }
 
