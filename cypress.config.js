4dde6a500485093671731518188aafe50e4ce6d2
Dan Garner <dan@xibosignage.com>
2022-10-10 20:15:37 +0100

    Release: campaigns grid only needs to wait for 3 reloads now (because we removed on change)

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2022 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
@@ -7,8 +28,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   requestTimeout: 10000,
   env: {
-      client_id: "MrGPc7e3IL1hA6w13l7Ru5giygxmNiafGNhFv89d",
-      client_secret: "Pk6DdDgu2HzSoepcMHRabY60lDEvQ9ucTejYvc5dOgNVSNaOJirCUM83oAzlwe0KBiGR2Nhi6ltclyNC1rmcq0CiJZXzE42KfeatQ4j9npr6nMIQAzMal8O8RiYrIoono306CfyvSSJRfVfKExIjj0ZyE4TUrtPezJbKmvkVDzh8aj3kbanDKatirhwpfqfVdfgsqVNjzIM9ZgKHnbrTX7nNULL3BtxxNGgDMuCuvKiJFrLSyIIz1F4SNrHwHz"
+      client_id: "7dd456e9e26d128e0c9d61045212d0afbcd5ea64",
+      client_secret: "9004c8edaea588a16ea8c4334d41683ccc9af48036b0d798da5ab8d038e69704e8d768483ea0c7870058cdf60f1c320386bd8702413c226d0ba75dcb617e2dc1f7693cb4e285bbe81d1cf1a5cbf8c7f9288a4ff912b5c73d59fe9f4f7b7e3383e5d276ccbdd1302c3be8acd176fa3d07aaa8dc0903a825d98eec0ee94e10b4"
   },
   e2e: {
     // We've imported your old cypress plugins here.
