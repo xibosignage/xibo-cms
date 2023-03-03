408356188db2101f7f90442d0be330a1d64b6551
ifarzana <m2farzana@gmail.com>
2023-03-03 16:37:30 +0000

    Fix migration mistake

--- db/migrations/20230214135035_remove_dooh_user_type_migration.php
+++ db/migrations/20230214135035_remove_dooh_user_type_migration.php
@@ -31,7 +31,7 @@ class RemoveDoohUserTypeMigration extends AbstractMigration
     public function change()
     {
         // Update all DOOH users to super admins
-        $this->execute('UPDATE `user` SET userTypeId = 1 WHERE userTypeId = 4)');
+        $this->execute('UPDATE `user` SET userTypeId = 1 WHERE userTypeId = 4');
 
         $this->execute('DELETE FROM `usertype` WHERE `userType` = \'DOOH\' ');
 
