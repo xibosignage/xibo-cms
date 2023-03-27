bedc240fd0b4c7126663f2baae90459f70ddafda
Israt Jahan Farzana <m2farzana@gmail.com>
2023-03-27 14:51:48 +0100

    Regions: adding a second media item to a zone should convert to playlist (#1692)

--- lib/Dependencies/Controllers.php
+++ lib/Dependencies/Controllers.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Copyright (C) 2022 Xibo Signage Ltd
+ * Copyright (C) 2023 Xibo Signage Ltd
  *
- * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Xibo - Digital Signage - https://xibosignage.com
  *
  * This file is part of Xibo.
  *
@@ -397,7 +397,8 @@ class Controllers
                     $c->get('layoutFactory'),
                     $c->get('displayFactory'),
                     $c->get('scheduleFactory'),
-                    $c->get('folderFactory')
+                    $c->get('folderFactory'),
+                    $c->get('regionFactory')
                 );
                 $controller->useBaseDependenciesService($c->get('ControllerBaseDependenciesService'));
                 return $controller;
