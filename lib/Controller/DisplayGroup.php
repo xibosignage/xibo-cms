54d8ff096a6a641879f6d359868fc3cfd2e80e8d
Peter <peter@xibosignage.com>
2023-03-30 08:35:46 +0100

    Display Group : Dispatch load event on assign displaygroup

--- lib/Controller/DisplayGroup.php
+++ lib/Controller/DisplayGroup.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Copyright (c) 2022 Xibo Signage Ltd
+ * Copyright (C) 2022-2023 Xibo Signage Ltd
  *
- * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Xibo - Digital Signage - https://xibosignage.com
  *
  * This file is part of Xibo.
  *
@@ -1254,6 +1254,7 @@ class DisplayGroup extends Base
         }
 
         $displayGroup->load();
+        $this->getDispatcher()->dispatch(new DisplayGroupLoadEvent($displayGroup), DisplayGroupLoadEvent::$NAME);
 
         if (!$this->getUser()->checkEditable($displayGroup)) {
             throw new AccessDeniedException();
