6f9b7ab05a478708feb2c05135cf081d3b1681e0
Ruben Pingol <128448242+rubenpingol-xibo@users.noreply.github.com>
2023-04-03 16:04:10 +0800

    Widgets: Rich Text: remove source code option (#1706)

--- ui/src/vendor/ckeditor/config.js
+++ ui/src/vendor/ckeditor/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Subscript,Superscript';
+	config.removeButtons = 'Subscript,Superscript,Source';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
