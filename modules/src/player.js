a91ec3d45a74c9d240f4f7cc92af95e6e77de58b
Ruben Pingol <ruben.pingol@xibosignage.com>
2023-04-20 15:10:03 +0800

    Layout Preview: Element with data preview

--- modules/src/player.js
+++ modules/src/player.js
@@ -77,6 +77,248 @@ $(function() {
       return dateNow.subtract(...params(subtractRegex)).format(utcFormat);
     }
   };
+  const elementsWidget = {};
+
+  /**
+   * Compose final data
+   * @param {object} widget
+   * @param {object|array} data
+   * @return {object}
+   */
+  function composeFinalData(widget, data) {
+    let dataItems = [];
+    let isSampleData = false;
+    const isArray = Array.isArray(data);
+
+    // If the request failed, and we're in preview, show the error message
+    if ((!widget.isValid ||
+        (!isArray && data.success === false)
+    ) && isPreview) {
+      if (widget.sample) {
+        isSampleData = true;
+        // If data is empty, use sample data instead
+        // Add single element or array of elements
+        dataItems = (Array.isArray(widget.sample)) ?
+          widget.sample.slice(0) : [widget.sample];
+
+        dataItems = dataItems.reduce((data, item) => {
+          Object.keys(item).forEach((itemKey) => {
+            if (String(item[itemKey]).match(macroRegex) !== null) {
+              item[itemKey] = composeUTCDateFromMacro(item[itemKey]);
+            }
+          });
+
+          return [...data, {...item}];
+        }, []);
+      }
+    } else {
+      dataItems = data?.data || [];
+    }
+
+    return {
+      isSampleData,
+      dataItems,
+      isArray,
+    };
+  }
+
+  /**
+   * Get widget data
+   * @param {object} widget
+   * @return {Promise}
+   */
+  function getWidgetData(widget) {
+    return new Promise(function(resolve) {
+      // if we have data on the widget (for older players),
+      // or if we are not in preview and have empty data on Widget (like text)
+      // do not run ajax use that data instead
+      if (
+        (widget.data.data !== undefined && widget.data.data.length > 0) ||
+        (widget.data.length == 0 && !isPreview)
+      ) {
+        resolve(widget.data);
+      } else {
+        // else get data from widget.url,
+        // this will be either getData for preview
+        // or new json file for v4 players
+        $.ajax({
+          method: 'GET',
+          url: widget.url,
+        }).done(function(data) {
+          resolve(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR, textStatus, errorThrown);
+        });
+      }
+    });
+  }
+
+  /**
+   * Initialize the player
+   * @param {object} $template
+   * @param {object} $content
+   * @param {boolean} moduleTemplate
+   * @param {object} hbs
+   * @param {object} widget
+   * @param {*} data
+   */
+  function initPlayer(
+    $template,
+    $content,
+    moduleTemplate,
+    hbs,
+    widget,
+    data,
+  ) {
+    widget.items = [];
+    const $target = $('body');
+    const {
+      isArray,
+      isSampleData,
+      dataItems,
+    } = composeFinalData(widget, data);
+
+    if (isSampleData) {
+      $target.append(
+        '<div class="error-message" role="alert">' +
+        data.message +
+        '</div>');
+    }
+
+    // Add meta to the widget if it exists
+    if (!isArray && data.meta) {
+      widget.meta = data.meta;
+    }
+
+    // Run the onInitialize function if it exists
+    if (typeof window['onInitialize_' + widget.widgetId] === 'function') {
+      window['onInitialize_' + widget.widgetId](
+        widget.widgetId,
+        $target,
+        widget.properties,
+        widget.meta,
+      );
+    }
+
+    // For each data item, parse it and add it to the content
+    let templateAlreadyAdded = false;
+    $.each(dataItems, function(_key, item) {
+      // Parse the data if there is a parser function
+      if (typeof window['onParseData_' + widget.widgetId] === 'function') {
+        item = window[
+          'onParseData_' + widget.widgetId
+        ](item, widget.properties, widget.meta);
+      }
+
+      // Add the item to the content
+      (hbs) && $content.append(hbs(item));
+
+      // Add items to the widget object
+      (item) && widget.items.push(item);
+
+      // IF we added item template
+      templateAlreadyAdded = true;
+    });
+
+    // If we don't have dataType, or we have a module template
+    // add it to the content with widget properties and global options
+    if (moduleTemplate && hbs && !templateAlreadyAdded) {
+      $content.append(hbs(
+        Object.assign(widget.properties, globalOptions),
+      ));
+    }
+
+    // Save template height and width if exists to global options
+    if ($template && $template.length > 0) {
+      globalOptions.widgetDesignWidth = $template.data('width');
+      globalOptions.widgetDesignHeight = $template.data('height');
+      globalOptions.widgetDesignGap = $template.data('gap');
+    }
+
+    // Save template properties to widget properties
+    for (const key in widget.templateProperties) {
+      if (widget.templateProperties.hasOwnProperty(key)) {
+        widget.properties[key] = widget.templateProperties[key];
+      }
+    }
+
+    // Run the onRender function if it exists
+    if (typeof window['onRender_' + widget.widgetId] === 'function') {
+      window.onRender =
+        window['onRender_' + widget.widgetId];
+    }
+
+    // Handle the rendering of the template
+    if (
+      typeof window['onTemplateRender_' + widget.templateId] === 'function'
+    ) { // Custom scaler
+      window.onTemplateRender =
+          window['onTemplateRender_' + widget.templateId];
+    }
+
+    // Create global render array of functions
+    window.renders = [];
+    // Template render function
+    if (window.onTemplateRender) {
+      // Save the render method in renders
+      window.renders.push(window.onTemplateRender);
+    }
+
+    // Module render function
+    if (window.onRender) {
+      // Save the render method in renders
+      window.renders.push(window.onRender);
+    }
+
+    // If there's no elements in renders, use the default scaler
+    if (window.renders.length === 0) {
+      window.renders.push(defaultScaler);
+    }
+
+    // Run all render functions
+    const optionsForRendering = Object.assign(
+      widget.properties,
+      globalOptions,
+      {
+        duration: widget.duration,
+      },
+    );
+    $.each(window.renders, function(_key, render) {
+      render(
+        widget.widgetId,
+        $target,
+        widget.items,
+        optionsForRendering,
+        widget.meta,
+      );
+    });
+
+    // Save widget as global variable
+    window.widget = widget;
+
+    // Call the run on visible function if it exists
+    if (
+      typeof window['onVisible_' + widget.widgetId] === 'function'
+    ) {
+      window.runOnVisible = function() {
+        window['onVisible_' + widget.widgetId](
+          widget.widgetId,
+          $target,
+          widget.items,
+          widget.properties,
+          widget.meta,
+        );
+      };
+      if (xiboIC.checkVisible()) {
+        window.runOnVisible();
+      } else {
+        xiboIC.addToQueue(window.runOnVisible);
+      }
+    }
+
+    // Lock all interactions
+    xiboIC.lockAllInteractions();
+  }
 
   // Call the data url and parse out the template.
   $.each(widgetData, function(_key, widget) {
@@ -102,201 +344,21 @@ $(function() {
 
     const $content = $('#content');
 
-    // if we have data on the widget (for older players),
-    // or if we are not in preview and have empty data on Widget (like text)
-    // do not run ajax use that data instead
-    if (
-      (widget.data.data !== undefined && widget.data.data.length > 0) ||
-      (widget.data.length == 0 && !isPreview)
-    ) {
-      initPlayer(widget.data);
-    } else {
-      // else get data from widget.url,
-      // this will be either getData for preview
-      // or new json file for v4 players
-      $.ajax({
-        method: 'GET',
-        url: widget.url,
-      }).done(function(data) {
-        initPlayer(data);
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR, textStatus, errorThrown);
-      });
+    // Get widget with templateId = elements
+    if (widget.templateId === 'elements') {
+      elementsWidget[widget.widgetId] = widget;
     }
 
-    /**
-     * Initialize the player
-     * @param {*} data
-     */
-    function initPlayer(data) {
-      const $target = $('body');
-      let dataItems = [];
-      widget.items = [];
-      const isArray = Array.isArray(data);
-
-      // If the request failed, and we're in preview, show the error message
-      if ((!widget.isValid ||
-          (!isArray && data.success === false)
-      ) && isPreview) {
-        $target.append(
-          '<div class="error-message" role="alert">' +
-          data.message +
-          '</div>');
-
-        if (widget.sample) {
-          // If data is empty, use sample data instead
-          // Add single element or array of elements
-          dataItems = (Array.isArray(widget.sample)) ?
-            widget.sample.slice(0) : [widget.sample];
-
-          dataItems = dataItems.reduce((data, item) => {
-            Object.keys(item).forEach((itemKey) => {
-              if (String(item[itemKey]).match(macroRegex) !== null) {
-                item[itemKey] = composeUTCDateFromMacro(item[itemKey]);
-              }
-            });
-
-            return [...data, {...item}];
-          }, []);
-        }
-      } else {
-        dataItems = data?.data || [];
-      }
-
-      // Add meta to the widget if it exists
-      if (!isArray && data.meta) {
-        widget.meta = data.meta;
-      }
-
-      // Run the onInitialize function if it exists
-      if (typeof window['onInitialize_' + widget.widgetId] === 'function') {
-        window['onInitialize_' + widget.widgetId](
-          widget.widgetId,
-          $target,
-          widget.properties,
-          widget.meta,
-        );
-      }
-
-      // For each data item, parse it and add it to the content
-      let templateAlreadyAdded = false;
-      $.each(dataItems, function(_key, item) {
-        // Parse the data if there is a parser function
-        if (typeof window['onParseData_' + widget.widgetId] === 'function') {
-          item = window[
-            'onParseData_' + widget.widgetId
-          ](item, widget.properties, widget.meta);
-        }
-
-        // Add the item to the content
-        (hbs) && $content.append(hbs(item));
-
-        // Add items to the widget object
-        (item) && widget.items.push(item);
-
-        // IF we added item template
-        templateAlreadyAdded = true;
-      });
-
-      // If we don't have dataType, or we have a module template
-      // add it to the content with widget properties and global options
-      if (moduleTemplate && hbs && !templateAlreadyAdded) {
-        $content.append(hbs(
-          Object.assign(widget.properties, globalOptions),
-        ));
-      }
-
-      // Save template height and width if exists to global options
-      if ($template && $template.length > 0) {
-        globalOptions.widgetDesignWidth = $template.data('width');
-        globalOptions.widgetDesignHeight = $template.data('height');
-        globalOptions.widgetDesignGap = $template.data('gap');
-      }
-
-      // Save template properties to widget properties
-      for (const key in widget.templateProperties) {
-        if (widget.templateProperties.hasOwnProperty(key)) {
-          widget.properties[key] = widget.templateProperties[key];
-        }
-      }
-
-      // Run the onRender function if it exists
-      if (typeof window['onRender_' + widget.widgetId] === 'function') {
-        window.onRender =
-          window['onRender_' + widget.widgetId];
-      }
-
-      // Handle the rendering of the template
-      if (
-        typeof window['onTemplateRender_' + widget.templateId] === 'function'
-      ) { // Custom scaler
-        window.onTemplateRender =
-          window['onTemplateRender_' + widget.templateId];
-      }
-
-      // Create global render array of functions
-      window.renders = [];
-      // Template render function
-      if (window.onTemplateRender) {
-        // Save the render method in renders
-        window.renders.push(window.onTemplateRender);
-      }
-
-      // Module render function
-      if (window.onRender) {
-        // Save the render method in renders
-        window.renders.push(window.onRender);
-      }
-
-      // If there's no elements in renders, use the default scaler
-      if (window.renders.length === 0) {
-        window.renders.push(defaultScaler);
-      }
-
-      // Run all render functions
-      const optionsForRendering = Object.assign(
-        widget.properties,
-        globalOptions,
-        {
-          duration: widget.duration,
-        },
+    getWidgetData(widget).then(function(data) {
+      initPlayer(
+        $template,
+        $content,
+        moduleTemplate,
+        hbs,
+        widget,
+        data,
       );
-      $.each(window.renders, function(_key, render) {
-        render(
-          widget.widgetId,
-          $target,
-          widget.items,
-          optionsForRendering,
-          widget.meta,
-        );
-      });
-
-      // Save widget as global variable
-      window.widget = widget;
-
-      // Call the run on visible function if it exists
-      if (
-        typeof window['onVisible_' + widget.widgetId] === 'function'
-      ) {
-        window.runOnVisible = function() {
-          window['onVisible_' + widget.widgetId](
-            widget.widgetId,
-            $target,
-            widget.items,
-            widget.properties,
-            widget.meta,
-          );
-        };
-        if (xiboIC.checkVisible()) {
-          window.runOnVisible();
-        } else {
-          xiboIC.addToQueue(window.runOnVisible);
-        }
-      }
-
-      // Lock all interactions
-      xiboIC.lockAllInteractions();
-    }
+    });
   });
 
   // Parse out the template with elements.
@@ -322,7 +384,7 @@ $(function() {
             }
 
             const elementProperties = {};
-            if (element.properties.length > 0) {
+            if (element.properties?.length > 0) {
               // Map properties as key:value pair.
               $.each(element.properties, function(_idx, property) {
                 if (!elementProperties.hasOwnProperty(property.id)) {
@@ -331,14 +393,42 @@ $(function() {
               });
             }
 
-            if (hbs) {
+            let templateData = Object.assign({}, elementProperties);
+            const renderElement = (data) => {
               const hbsTemplate = hbs(
-                Object.assign(elementProperties, globalOptions),
+                Object.assign(data, globalOptions),
               );
 
               $content.append($(hbsTemplate).first().xiboElementsRender(
                 Object.assign(elementProperties, element, globalOptions),
               ).prop('outerHTML'));
+            };
+
+            // Get widget info if exists.
+            if (Object.keys(elementsWidget).length > 0 &&
+              elementsWidget.hasOwnProperty(widgetElement.widgetId)
+            ) {
+              const widgetInfo = elementsWidget[widgetElement.widgetId];
+              getWidgetData(widgetInfo)
+                .then(function(data) {
+                  const {
+                    dataItems,
+                  } = composeFinalData(widgetInfo, data);
+
+                  if (dataItems.length > 0) {
+                    templateData = Object.assign(
+                      templateData, {data: dataItems[0]},
+                    );
+                  }
+
+                  if (hbs) {
+                    renderElement(templateData);
+                  }
+                });
+            } else {
+              if (hbs) {
+                renderElement(templateData);
+              }
             }
           });
         }
