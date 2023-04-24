f787f3d3be5809f2c118b57ad705b9d873df7a6c
ifarzana <m2farzana@gmail.com>
2023-04-24 15:53:03 +0100

    fix

--- lib/Factory/LayoutFactory.php
+++ lib/Factory/LayoutFactory.php
@@ -1425,7 +1425,7 @@ class LayoutFactory extends BaseFactory
                         $upgraded = $widgetCompatibilityInterface->upgradeWidget($widget, $widget->schemaVersion, 2);
                         if ($upgraded) {
                             $widget->schemaVersion = 2;
-                            $widget->save(['alwaysUpdate'=>true, notifyDisplays => false]);
+                            $widget->save(['alwaysUpdate'=>true, 'notifyDisplays' => false]);
                         }
                     } catch (\Exception $e) {
                         $this->getLog()->error('Error upgrading widget '. $e->getMessage());
