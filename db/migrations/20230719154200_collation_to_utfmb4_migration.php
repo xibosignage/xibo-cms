65ce627e74a4979a9f7fd700fea0b824131326f2
Dan Garner <dan@xibosignage.com>
2023-07-23 10:42:22 +0100

    Env: solve issue with migrating utf8mb4 xibosignage/xibo#3044

--- db/migrations/20230719154200_collation_to_utfmb4_migration.php
+++ db/migrations/20230719154200_collation_to_utfmb4_migration.php
@@ -30,6 +30,18 @@ class CollationToUtfmb4Migration extends AbstractMigration
 {
     public function change(): void
     {
+        // Several tables have keys on varchar which will need to be resized to accomodate the max key length
+        // of MySQL 5.6 which is 767 bytes. This will mean 191.
+        $this->table('menu_product_options')->changeColumn('option', 'string', [
+            'limit' => 191,
+        ])->save();
+
+        $this->table('widgetoption')->changeColumn('option', 'string', [
+            'limit' => 191,
+        ])->save();
+
+        // Get all tables which need to have their collation converted.
+        // We will exclude some tables which have foreign keys on string columns.
         $tables = $this->fetchAll('
             SELECT TABLE_NAME
             FROM INFORMATION_SCHEMA.TABLES
@@ -37,6 +49,12 @@ class CollationToUtfmb4Migration extends AbstractMigration
               AND `TABLE_TYPE` = \'BASE TABLE\'
                 AND `ENGINE` = \'InnoDB\'
                 AND TABLE_COLLATION <> \'utf8mb4_general_ci\'
+                AND `TABLE_NAME` NOT IN (
+                    \'oauth_clients\',
+                    \'oauth_scopes\',
+                    \'oauth_client_scopes\',
+                    \'oauth_lkclientuser\'
+                )
         ');
 
         foreach ($tables as $row) {
