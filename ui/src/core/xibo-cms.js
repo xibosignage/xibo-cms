5151d3b7333abf65c7e794a053d35adb156844ff
Peter <peter@xibosignage.com>
2023-07-28 14:20:52 +0100

    Grid Filtering : Fix tag filter xibosignage/xibo#3063

--- ui/src/core/xibo-cms.js
+++ ui/src/core/xibo-cms.js
@@ -163,6 +163,9 @@ function XiboInitialise(scope, options) {
         // Folder navigation relies on triggering the change event on this hidden field.
         $(this).find('.XiboFilter form #folderId').on('change', filterRefresh);
 
+        // Tags need on change trigger.
+        $(this).find('.XiboFilter form input[data-role="tagsInputInline"]').on('change', filterRefresh);
+
         // check to see if we need to share folder tree state globally or per page
         var gridFolderState = rememberFolderTreeStateGlobally ? 'grid-folder-tree-state' : 'grid_'+gridName ;
         // init the jsTree
