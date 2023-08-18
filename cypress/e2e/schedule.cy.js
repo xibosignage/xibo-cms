d29c314734fbac90894e86ec5eea7f4ba44f7e77
ifarzana <m2farzana@gmail.com>
2023-08-18 17:38:54 +0100

    separate tests for easier debugging

--- cypress/e2e/schedule.cy.js
+++ cypress/e2e/schedule.cy.js
@@ -38,8 +38,48 @@ describe('Campaigns', function() {
     });
   });
 
-  it('should schedule an event campaign/layout/command/overlay layout that has no priority, no recurrence', function() {
+  it('should schedule an event campaign that has no priority, no recurrence', function() {
+    // Set up intercepts with aliases
+    cy.intercept('/displaygroup?*').as('loadDisplaygroups');
+    cy.intercept('/campaign?type=list*').as('loadListCampaigns');
+    cy.intercept('/campaign?isLayoutSpecific=-1*').as('loadLayoutSpecificCampaign');
+    cy.intercept('/display?start=*').as('loadDisplays');
+    cy.intercept('/schedule?draw=4&*').as('scheduleGridLoad');
+    cy.intercept('/layout?*').as('layoutLoad');
+    cy.intercept('/user/pref').as('userPref');
+    cy.intercept('POST', '/schedule').as('postCampaign'); // Intercept POST request
 
+    // Create a campaign
+    cy.createCampaign('Campaign for Schedule 1');
+
+    // Visit the page and click on the Add Event button
+    cy.visit('/schedule/view');
+    cy.contains('Add Event').click();
+
+    // Fill event details
+    cy.get('.col-sm-10 > #eventTypeId').select('Campaign', {force: true});
+    cy.get(':nth-child(3) > .col-sm-10 > .select2 > .selection > .select2-selection > .select2-selection__rendered')
+      .type('List Campaign Display 1');
+
+    // Wait for Display to load and select the display
+    cy.wait('@loadDisplaygroups');
+    cy.get('.select2-container--open').contains('List Campaign Display 1');
+    cy.get('.select2-container--open .select2-dropdown .select2-results > ul > li').should('have.length', 2);
+    cy.get('#select2-displayGroupIds-results > li > ul > li:first').contains('List Campaign Display 1').click();
+
+    // Select day part and campaign
+    cy.get('[name="dayPartId"]').select('Always', {force: true});
+    cy.get('.layout-control > .col-sm-10 > .select2 > .selection > .select2-selection').type('Campaign for Schedule 1');
+    cy.wait('@loadListCampaigns');
+    cy.get('.select2-container--open').contains('Campaign for Schedule 1');
+    cy.get('.select2-container--open .select2-results > ul > li:first').click();
+
+    // Click Next and check toast message
+    cy.get('.modal .modal-footer').contains('Next').click();
+    cy.contains('Added Event');
+  });
+
+  it('should schedule an event layout that has no priority, no recurrence', function() {
     cy.intercept('/displaygroup?*').as('loadDisplaygroups');
     cy.intercept('/campaign?type=list*').as('loadListCampaigns');
     cy.intercept('/campaign?isLayoutSpecific=-1*').as('loadLayoutSpecificCampaign');
@@ -48,43 +88,24 @@ describe('Campaigns', function() {
     cy.intercept('/layout?*').as('layoutLoad');
     cy.intercept('/user/pref').as('userPref');
 
-    cy.createCampaign('Campaign for Schedule 1');
-    cy.createCommand('Set Timezone', 'Set timezone', 'TIMEZONE');
-
     // Intercept the POST request to get the schedule Id
     cy.intercept('/schedule').as('postCampaign');
 
+    // Click on the Add Event button
     cy.visit('/schedule/view');
     cy.contains('Add Event').click();
 
-    cy.get('.col-sm-10 > #eventTypeId').select('Campaign', {force: true});
+    // Layout
+    cy.get('.col-sm-10 > #eventTypeId').select('Layout', {force: true});
     cy.get(':nth-child(3) > .col-sm-10 > .select2 > .selection > .select2-selection > .select2-selection__rendered')
       .type('List Campaign Display 1');
-    // Wait for Display to load
+    // Wait for Display to load and select the display
     cy.wait('@loadDisplaygroups');
     cy.get('.select2-container--open').contains('List Campaign Display 1');
     cy.get('.select2-container--open .select2-dropdown .select2-results > ul > li').should('have.length', 2);
     cy.get('#select2-displayGroupIds-results > li > ul > li:first').contains('List Campaign Display 1').click();
-    // cy.get('[name="dayPartId"]').select('Daypart 11-14', {force: true});
     cy.get('[name="dayPartId"]').select('Always', {force: true});
 
-    // Select Campaign
-    cy.get('.layout-control > .col-sm-10 > .select2 > .selection > .select2-selection')
-      .type('Campaign for Schedule 1');
-    // Wait for Campaign to load
-    cy.wait('@loadListCampaigns');
-    cy.get('.select2-container--open').contains('Campaign for Schedule 1');
-    // cy.get('.select2-container--open .select2-dropdown .select2-results > ul > li').should('have.length', 1);
-    cy.get('.select2-container--open .select2-results > ul > li').should('have.length', 1);
-    cy.get('.select2-container--open .select2-results > ul > li:first').contains('Campaign for Schedule 1').click();
-    cy.get('.modal .modal-footer').contains('Next').click();
-
-    // Check toast message
-    cy.contains('Added Event');
-    // ---------
-
-    // Layout
-    cy.get('.col-sm-10 > #eventTypeId').select('Layout', {force: true});
     // Select Layout
     cy.get('.layout-control > .col-sm-10 > .select2 > .selection > .select2-selection')
       .type('Layout for Schedule 1');
@@ -95,11 +116,36 @@ describe('Campaigns', function() {
     cy.get('.select2-container--open .select2-results > ul > li').should('have.length', 1);
     cy.get('.select2-container--open .select2-results > ul > li:first').contains('Layout for Schedule 1').click();
     cy.get('.modal .modal-footer').contains('Next').click();
+    cy.contains('Added Event');
+  });
 
-    // ---------
+  it('should schedule an event command/overlay layout that has no priority, no recurrence', function() {
+    cy.intercept('/displaygroup?*').as('loadDisplaygroups');
+    cy.intercept('/campaign?type=list*').as('loadListCampaigns');
+    cy.intercept('/campaign?isLayoutSpecific=-1*').as('loadLayoutSpecificCampaign');
+    cy.intercept('/display?start=*').as('loadDisplays');
+    cy.intercept('/schedule?draw=4&*').as('scheduleGridLoad');
+    cy.intercept('/layout?*').as('layoutLoad');
+    cy.intercept('/user/pref').as('userPref');
+
+    // Intercept the POST request to get the schedule Id
+    cy.intercept('/schedule').as('postCampaign');
+
+    cy.createCommand('Set Timezone', 'Set timezone', 'TIMEZONE');
+
+    // Click on the Add Event button
+    cy.visit('/schedule/view');
+    cy.contains('Add Event').click();
 
     // Create Command Schedule
     cy.get('.col-sm-10 > #eventTypeId').select('Command', {force: true});
+    cy.get(':nth-child(3) > .col-sm-10 > .select2 > .selection > .select2-selection > .select2-selection__rendered')
+      .type('List Campaign Display 1');
+    // Wait for Display to load and select the display
+    cy.wait('@loadDisplaygroups');
+    cy.get('.select2-container--open').contains('List Campaign Display 1');
+    cy.get('.select2-container--open .select2-dropdown .select2-results > ul > li').should('have.length', 2);
+    cy.get('#select2-displayGroupIds-results > li > ul > li:first').contains('List Campaign Display 1').click();
 
     cy.get('.starttime-control > .col-sm-10 > .input-group > .datePickerHelper').click();
     cy.get('.open > .flatpickr-innerContainer > .flatpickr-rContainer > .flatpickr-days > .dayContainer > .today').click();
@@ -130,6 +176,19 @@ describe('Campaigns', function() {
     cy.get('#select2-displayGroupIds-results > li > ul > li:first').contains('List Campaign Display 1').click();
 
     cy.get('.modal .modal-footer').contains('Save').click();
+  });
+
+  it('schedule creation should be successful', function() {
+    cy.intercept('/displaygroup?*').as('loadDisplaygroups');
+    cy.intercept('/campaign?type=list*').as('loadListCampaigns');
+    cy.intercept('/campaign?isLayoutSpecific=-1*').as('loadLayoutSpecificCampaign');
+    cy.intercept('/display?start=*').as('loadDisplays');
+    cy.intercept('/schedule?draw=4&*').as('scheduleGridLoad');
+    cy.intercept('/layout?*').as('layoutLoad');
+    cy.intercept('/user/pref').as('userPref');
+
+    // Intercept the POST request to get the schedule Id
+    cy.intercept('/schedule').as('postCampaign');
 
     // ------
     // Check if schedule creation was successful
@@ -186,15 +245,15 @@ describe('Campaigns', function() {
 
     // ---------
     // Delete the schedule
-    // cy.get('#schedule-grid tbody tr').should('have.length', 2);
-    // cy.wait('@scheduleGridLoad');
-    // cy.wait('@userPref');
-    // cy.wait('@scheduleGridLoad');
-    // cy.get('#schedule-grid tr:first-child .dropdown-toggle').click();
-    // cy.get('#schedule-grid tr:first-child .schedule_button_delete').click();
-    // cy.get('.bootbox .save-button').click();
-    //
-    // // Validate the schedule no longer exist
-    // cy.get('#schedule-grid tbody tr').should('have.length', 1);
+    cy.get('#schedule-grid tbody tr').should('have.length', 2);
+    cy.wait('@scheduleGridLoad');
+    cy.wait('@userPref');
+    cy.wait('@scheduleGridLoad');
+    cy.get('#schedule-grid tr:first-child .dropdown-toggle').click();
+    cy.get('#schedule-grid tr:first-child .schedule_button_delete').click();
+    cy.get('.bootbox .save-button').click();
+
+    // Validate the schedule no longer exist
+    cy.get('#schedule-grid tbody tr').should('have.length', 1);
   });
 });
