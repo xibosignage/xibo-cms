7e816061f42e2a5bcda1ddc1148b2e473de0ba2c
Dan Garner <dan@xibosignage.com>
2023-08-26 13:19:27 +0100

    Welcome page - remove old tours, reword and reformat page. xibosignageltd/xibo-private#429

--- ui/bundle_xibo.js
+++ ui/bundle_xibo.js
@@ -20,11 +20,6 @@
  */
 
 // --- Xibo JS files ----
-
-// Xibo Tour ( based on bootstrap-tour )
-require('./src/style/tour.scss');
-require('./src/core/tour.js');
-
 // Xibo forms
 require('./src/style/forms.scss');
 require('./src/core/forms.js');
