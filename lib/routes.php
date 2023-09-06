f06bc5ae3d49b4e58f12b258481e024e305d24c9
Dan Garner <dan@xibosignage.com>
2023-09-06 19:31:03 +0100

    Feature fixes: fonts/sync groups (#2057)
    Feature flags shouldn't disable getting a list of those things (only ones you have sharing for).
    
    * Unable to edit text widget:  xibosignage/xibo#3118
    * Sync groups: should be able to view sync groups (shared with you) in filters, etc, without viewing the sync group page.

--- lib/routes.php
+++ lib/routes.php
@@ -835,17 +835,18 @@ $app->group('', function (RouteCollectorProxy $group) {
 })
     ->addMiddleware(new FeatureAuth($app->getContainer(), ['menuBoard.modify']));
 
+$app->get('/fonts', ['\Xibo\Controller\Font', 'grid'])->setName('font.search');
 $app->group('', function (RouteCollectorProxy $group) {
-    $group->get('/fonts', ['\Xibo\Controller\Font', 'grid'])->setName('font.search');
     $group->get('/fonts/details/{id}', ['\Xibo\Controller\Font', 'getFontLibDetails'])->setName('font.details');
     $group->get('/fonts/download/{id}', ['\Xibo\Controller\Font', 'download'])->setName('font.download');
     $group->post('/fonts', ['\Xibo\Controller\Font','add'])->setName('font.add');
     $group->delete('/fonts/{id}/delete', ['\Xibo\Controller\Font','delete'])->setName('font.delete');
 })->addMiddleware(new FeatureAuth($app->getContainer(), ['font.view']));
 
+$app->get('/syncgroups', ['\Xibo\Controller\SyncGroup', 'grid'])->setName('syncgroup.search');
 $app->group('', function (RouteCollectorProxy $group) {
-    $group->get('/syncgroups', ['\Xibo\Controller\SyncGroup', 'grid'])->setName('syncgroup.search');
-    $group->get('/syncgroup/{id}/displays', ['\Xibo\Controller\SyncGroup', 'fetchDisplays'])->setName('syncgroup.fetch.displays');
+    $group->get('/syncgroup/{id}/displays', ['\Xibo\Controller\SyncGroup', 'fetchDisplays'])
+        ->setName('syncgroup.fetch.displays');
     $group->post('/syncgroup/add', ['\Xibo\Controller\SyncGroup', 'add'])->setName('syncgroup.add');
     $group->post('/syncgroup/{id}/members', ['\Xibo\Controller\SyncGroup', 'members'])->setName('syncgroup.members');
     $group->put('/syncgroup/{id}/edit', ['\Xibo\Controller\SyncGroup', 'edit'])->setName('syncgroup.edit');
