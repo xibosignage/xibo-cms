378639e84458328080f256f12cac219ac38e2961
Peter <peter@xibosignage.com>
2023-10-02 12:39:22 +0100

    DataSets : Fix issue with displayGeoLocation replacement on formula validation.

--- lib/Entity/DataSetColumn.php
+++ lib/Entity/DataSetColumn.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Copyright (c) 2022 Xibo Signage Ltd
+ * Copyright (C) 2023 Xibo Signage Ltd
  *
- * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Xibo - Digital Signage - https://xibosignage.com
  *
  * This file is part of Xibo.
  *
@@ -277,6 +277,8 @@ class DataSetColumn implements \JsonSerializable
         if ($this->dataSetColumnTypeId == 2 && $this->formula != '' && substr($this->formula, 0, 1) !== '$') {
            try {
                $formula = str_replace('[DisplayId]', 0, $this->formula);
+               // replace DisplayGeoLocation with default CMS location, just to validate here.
+               $formula = str_replace('[DisplayGeoLocation]', "GEOMFROMTEXT('POINT(51.504 -0.104)')", $formula);
                $this->getStore()->select('SELECT * FROM (SELECT `id`, ' . $formula . ' AS `' . $this->heading . '`  FROM `dataset_' . $this->dataSetId . '`) dataset WHERE 1 = 1 ', []);
            } catch (\Exception $e) {
                $this->getLog()->debug('Formula validation failed with following message ' . $e->getMessage());
