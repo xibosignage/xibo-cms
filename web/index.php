60586d5908b4752606a78b0120c732b1efdd5927
Dan Garner <dan@xibosignage.com>
2024-03-19 17:17:18 +0000

    Strict CSP with fallback. Widget HTML renderer takes care of adding nonce for previewing.

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Copyright (c) 2022 Xibo Signage Ltd
+ * Copyright (C) 2024 Xibo Signage Ltd
  *
- * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Xibo - Digital Signage - https://xibosignage.com
  *
  * This file is part of Xibo.
  *
@@ -94,6 +94,7 @@ $app->add(new \Xibo\Middleware\ConnectorMiddleware($app));
 $app->add(new \Xibo\Middleware\ListenersMiddleware($app));
 $app->add(new \Xibo\Middleware\Theme($app));
 $app->add(new \Xibo\Middleware\CsrfGuard($app));
+$app->add(new \Xibo\Middleware\Csp($container));
 
 // Authentication
 $authentication = ($container->get('configService')->authentication != null)
