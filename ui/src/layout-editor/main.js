fc7e50b505e44fb32899fa959167e6320af5d39d
maurofmferrao <maurofmferrao@gmail.com>
2024-06-06 19:56:21 +0100

    Text Element: Inline editing relates to xibosignage/xibo#3255

--- ui/src/layout-editor/main.js
+++ ui/src/layout-editor/main.js
@@ -3552,6 +3552,14 @@ lD.openContextMenu = function(obj, position = {x: 0, y: 0}) {
     layoutObject.type === 'element-group'
   ) ? lD.getObjectByTypeAndId('widget', objAuxId, 'canvas') : {};
 
+  // Check if it's global text to be edited inline
+  const canEditText = (
+    layoutObject.type === 'element' &&
+    layoutObject.selected &&
+    layoutObject.elementType === 'global' &&
+    layoutObject.template.templateId === 'text'
+  );
+
   // Create menu and append to the designer div
   // ( using the object extended with translations )
   lD.editorContainer.append(
@@ -3561,6 +3569,7 @@ lD.openContextMenu = function(obj, position = {x: 0, y: 0}) {
       canHaveNewConfig: canHaveNewConfig,
       canChangeLayer: canChangeLayer,
       canUngroup: canUngroup,
+      canEditText: canEditText,
       widget: singleWidget,
       isElementBased: (
         layoutObject.type === 'element' ||
@@ -3881,6 +3890,8 @@ lD.openContextMenu = function(obj, position = {x: 0, y: 0}) {
           );
         });
       });
+    } else if (target.data('action') == 'editText') {
+      lD.viewer.editText(layoutObject);
     } else {
       const property = target.data('property');
       const propertyType = target.data('propertyType');
