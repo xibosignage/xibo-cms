4b607a4576265418665c15ac8a063f276cc878b6
Dan Garner <dan@xibosignage.com>
2024-07-03 15:08:28 +0100

    Upload: add back copyright which phpstorm stripped out xibosignageltd/xibo-private#740

--- lib/Helper/BlueImpUploadHandler.php
+++ lib/Helper/BlueImpUploadHandler.php
@@ -26,6 +26,14 @@ use Psr\Log\LoggerInterface;
 
 /**
  * Heavily modified BlueImp Upload handler, stripped out image processing, downloads, etc.
+ * jQuery File Upload Plugin PHP Class 6.4.2
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
  */
 class BlueImpUploadHandler
 {
