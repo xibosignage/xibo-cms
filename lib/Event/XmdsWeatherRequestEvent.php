23255ec90184ceeee482364de852f841f6bb749a
Nadz Mangandog <nadzmangandog@gmail.com>
2024-07-09 20:36:21 +0800

    Schedule: Criteria based scheduling - part 2 (#648)
    relates to xibosignageltd/xibo-private#648

---
+++ lib/Event/XmdsWeatherRequestEvent.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * Copyright (C) 2024 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - https://xibosignage.com
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Xibo\Event;
+
+/**
+ * Event raised when XMDS receives a request for weather data.
+ */
+class XmdsWeatherRequestEvent extends Event
+{
+    public static $NAME = 'xmds.weather.request';
+
+    /**
+     * @var float
+     */
+    private $latitude;
+
+    /**
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     */
+    private $weatherData;
+
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     */
+    public function __construct(float $latitude, float $longitude)
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude(): float
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude(): float
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Sets the weather data.
+     *
+     * @param string $weatherData
+     */
+    public function setWeatherData(string $weatherData): void
+    {
+        $this->weatherData = $weatherData;
+    }
+
+    /**
+     * Gets the weather data.
+     *
+     * @return string
+     */
+    public function getWeatherData(): string
+    {
+        return $this->weatherData;
+    }
+}
