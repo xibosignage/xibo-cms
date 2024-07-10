c4e230486c006edd5e4af527baf37dc29c351015
Mae Grace Baybay <87640602+mgbaybay@users.noreply.github.com>
2024-07-10 20:43:17 +0800

    Fixed onclick buttons throw CSP error (#2615)

--- lib/Controller/Library.php
+++ lib/Controller/Library.php
@@ -2147,7 +2147,8 @@ class Library extends Base
                     'id' => 'layout_button_preview',
                     'external' => true,
                     'url' => '#',
-                    'onclick' => 'createMiniLayoutPreview("' . $this->urlFor($request, 'layout.preview', ['id' => $layout->layoutId]) . '");',
+                    'onclick' => 'createMiniLayoutPreview',
+                    'onclickParam' => $this->urlFor($request, 'layout.preview', ['id' => $layout->layoutId]),
                     'text' => __('Preview Layout')
                 );
             }
