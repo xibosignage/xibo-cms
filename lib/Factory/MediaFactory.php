ddfda98f62a0b8a537fe208d2fe8d4b86f3ac603
Dan Garner <dan@xibosignage.com>
2024-12-23 15:13:14 +0000

    Library: fix filtering issue with media used as background images on layouts xibosignageltd/xibo-private#879

--- lib/Factory/MediaFactory.php
+++ lib/Factory/MediaFactory.php
@@ -791,9 +791,14 @@ class MediaFactory extends BaseFactory
                         ON widget.widgetId = lkwidgetmedia.widgetId
                      WHERE region.layoutId = :layoutId ';
 
-            // include Media only for non dynamic Playlists #2392
+            // include Media only for non-dynamic Playlists #2392
             if ($sanitizedFilter->getInt('excludeDynamicPlaylistMedia') === 1) {
-                $body .= ' AND lkplaylistplaylist.childId IN (SELECT playlistId FROM playlist WHERE playlist.playlistId = lkplaylistplaylist.childId AND playlist.isDynamic = 0) ';
+                $body .= ' AND lkplaylistplaylist.childId IN (
+                    SELECT playlistId 
+                      FROM playlist
+                     WHERE playlist.playlistId = lkplaylistplaylist.childId 
+                       AND playlist.isDynamic = 0
+                    ) ';
             }
 
             if ($sanitizedFilter->getInt('widgetId') !== null) {
@@ -801,13 +806,17 @@ class MediaFactory extends BaseFactory
                 $params['widgetId'] = $sanitizedFilter->getInt('widgetId');
             }
 
+            if ($sanitizedFilter->getInt('includeLayoutBackgroundImage') === 1) {
+                $body .= ' UNION ALL
+                    SELECT `layout`.backgroundImageId AS mediaId
+                      FROM `layout`
+                     WHERE `layout`.layoutId = :layoutId
+                    ';
+            }
+
             $body .= '    )
                 AND media.type <> \'module\'
             ';
-
-            if ($sanitizedFilter->getInt('includeLayoutBackgroundImage') === 1) {
-                $body .= ' OR media.mediaId IN ( SELECT `layout`.backgroundImageId FROM `layout` WHERE `layout`.layoutId = :layoutId ) ';
-            }
 
             $params['layoutId'] = $sanitizedFilter->getInt('layoutId');
         }
