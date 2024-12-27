218a81b55b000c695ab39404950d049377d1d8fe
Dan Garner <dan@xibosignage.com>
2024-12-27 08:37:09 +0000

    Errors: fix missing property for InvalidArgument.

--- lib/Helper/SanitizerService.php
+++ lib/Helper/SanitizerService.php
@@ -1,9 +1,23 @@
 <?php
-/**
- * Copyright (c) 2019 Xibo Signage Ltd
- * All Rights Reserved
+/*
+ * Copyright (C) 2024 Xibo Signage Ltd
  *
- * This is commercial proprietary software.
+ * Xibo - Digital Signage - https://xibosignage.com
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Xibo\Helper;
@@ -25,7 +39,6 @@ class SanitizerService
         return (new RespectSanitizer())
             ->setCollection($array)
             ->setDefaultOptions([
-                'throwClass' => '\Xibo\Support\Exception\InvalidArgumentException',
                 'checkboxReturnInteger' => true
             ]);
     }
