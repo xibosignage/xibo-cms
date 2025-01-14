a258b568e9b3f687583e027e5e8cf0b61e792636
Lykxa <92708658+qaxlyka@users.noreply.github.com>
2025-01-14 10:22:17 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -25,8 +25,8 @@ module.exports = defineConfig({
   viewportWidth: 1366,
   viewportHeight: 768,
   numTestsKeptInMemory: 5,
-  defaultCommandTimeout: 10000,
-  requestTimeout: 10000,
+  defaultCommandTimeout: 20000,
+  requestTimeout: 20000,
   env: {
       client_id: "MrGPc7e3IL1hA6w13l7Ru5giygxmNiafGNhFv89d",
       client_secret: "Pk6DdDgu2HzSoepcMHRabY60lDEvQ9ucTejYvc5dOgNVSNaOJirCUM83oAzlwe0KBiGR2Nhi6ltclyNC1rmcq0CiJZXzE42KfeatQ4j9npr6nMIQAzMal8O8RiYrIoono306CfyvSSJRfVfKExIjj0ZyE4TUrtPezJbKmvkVDzh8aj3kbanDKatirhwpfqfVdfgsqVNjzIM9ZgKHnbrTX7nNULL3BtxxNGgDMuCuvKiJFrLSyIIz1F4SNrHwHz"
