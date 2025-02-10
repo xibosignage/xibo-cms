4d3d00d7b2970752907dced21d07982dad704ad2
Charlyn <siaotongcharlyn@gmail.com>
2025-02-10 17:04:26 +0800

    Implement functionalities

--- ui/src/pages/welcome/welcome-page.js
+++ ui/src/pages/welcome/welcome-page.js
@@ -1,30 +1,78 @@
 import './welcome-page.scss';
 
-$(function() {
-  // TODO: debug to delete!
-  console.log('Welcome Page!');
-  console.log('Is it Xibo themed: ' + isXiboThemed);
+$(function () {
 
-  // Create Onboarding cards
   for (let index = 0; index < onboardingCard.length; index++) {
-    // Get card data
     const card = onboardingCard[index];
 
-    // Build HTML with template
     const $newCard = $(templates.welcome.welcomeCard(card));
 
-    // TODO: Handle click behaviour
-    $newCard.on('click', function() {
-      console.log('Go to serviceCard!');
+    $newCard.on('click', function (e) {
+      e.preventDefault();
+      var targetId = $(this).attr("href");
+      var targetElement = $(targetId);
+
+      if (targetElement.length) {
+        var offset = targetElement.offset().top - 100;
+
+        $("html, body").animate({
+          scrollTop: offset
+        }, 800);
+
+        targetElement.css({
+          "border": "3px solid #0e70f6",
+          "transition": "border-color 1s ease-out"
+        });
+
+        setTimeout(function () {
+          targetElement.css("border-color", "transparent");
+        }, 1000);
+      }
     });
 
-    // Add to container
     $newCard.appendTo('.welcome-page .onboarding-cards-container');
   }
 
-  // TODO: Create service cards
+  for (let index = 0; index < serviceCards.length; index++) {
+    const card = serviceCards[index];
 
-  // TODO: Create other cards
+    let targetContainer = null;
 
-  // TODO: Handle go to top button
+    if (card.featureFlag === "displays.view") {
+      targetContainer = ".service-card-container .displays-enabled";
+    } else if (Array.isArray(card.featureFlag) && card.featureFlag.includes("library.view") || card.featureFlag.includes("layout.view")) {
+      targetContainer = ".service-card-container .library-layout-enabled";
+    } else if (card.featureFlag === "schedule.view") {
+      targetContainer = ".service-card-container .schedule-enabled";
+    }
+
+    if (targetContainer && $(targetContainer).length) {
+      const $serviceCard = $(templates.welcome.serviceCard(card));
+      $serviceCard.appendTo(targetContainer);
+    }
+  }
+
+  for (let index = 0; index < othersCards.length; index++) {
+    const card = othersCards[index];
+
+    const $othersCard = $(templates.welcome.othersCard(card));
+
+    $othersCard.appendTo('.welcome-page .others-card-container');
+  }
+
+  var scrollUpButton = $(".scroll-up");
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 200) {
+      scrollUpButton.fadeIn();
+    } else {
+      scrollUpButton.fadeOut();
+    }
+  });
+
+  scrollUpButton.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "smooth");
+  });
+
 });
