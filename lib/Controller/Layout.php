6874817c65d93a6c6b99dd0345e508535c687d0e
mgbaybay <maegracebaybay.mb@gmail.com>
2025-03-13 22:49:45 +0800

    Fixed Schedule Video/Image: Video freezes instead of looping on player

--- lib/Controller/Layout.php
+++ lib/Controller/Layout.php
@@ -3590,6 +3590,12 @@ class Layout extends Base
         // Calculate the duration
         $widget->calculateDuration($module);
 
+        // Set loop for media items with custom duration
+        if ($type === 'media' && $media->mediaType === 'video' && $itemDuration > $media->duration) {
+            $widget->setOptionValue('loop', 'attrib', 1);
+            $widget->save();
+        }
+
         // Assign the widget to the playlist
         $region->getPlaylist()->assignWidget($widget);
         // Save the playlist
