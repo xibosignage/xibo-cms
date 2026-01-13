e51311dcfb993132e17e4c910c5c28843e85e558
Charlyn <siaotongcharlyn@gmail.com>
2026-01-13 17:16:52 +0800

    Add copyrights to Sidebar components

--- frontend/src/components/layout/SideBar.tsx
+++ frontend/src/components/layout/SideBar.tsx
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2026 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - https://xibosignage.com
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+.*/
+
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
@@ -8,7 +29,7 @@ import { SidebarPopup } from '../ui/sidebar/SidebarPopup';
 import { SidebarSubLinks } from '../ui/sidebar/SidebarSublinks';
 
 import { APP_ROUTES } from '@/config/appRoutes';
-import { isRouteActive } from '@/hooks/sidebar';
+import { isRouteActive } from '@/utils/sidebar';
 
 interface SidebarMenuProps {
   isCollapsed: boolean;
