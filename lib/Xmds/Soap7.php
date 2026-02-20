ac99366eda26cc406d40e76f6e4b0ca6e1ff6709
Dan Garner <dan@xibosignage.com>
2026-02-20 08:46:18 +0000

    PWA: fix for URLs output fixes xibosignage/xibo#3822

--- lib/Xmds/Soap7.php
+++ lib/Xmds/Soap7.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) 2025 Xibo Signage Ltd
+ * Copyright (C) 2026 Xibo Signage Ltd
  *
  * Xibo - Digital Signage - https://xibosignage.com
  *
@@ -222,6 +222,8 @@ class Soap7 extends Soap6
                                     'M',
                                     intval($row['mediaId']),
                                     $row['storedAs'],
+                                    null,
+                                    $this->display->isPwa(),
                                 ),
                             ];
                         }
