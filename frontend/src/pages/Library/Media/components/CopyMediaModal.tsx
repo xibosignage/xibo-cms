70131c544271a8278369bade62cd89ecaea27ab1
maurofmferrao <maurofmferrao@gmail.com>
2026-03-27 13:09:15 +0000

    V5 Copy media: Modal does not pre-populate tags from the original item relates to xibosignageltd/xibo-private#1278

--- frontend/src/pages/Library/Media/components/CopyMediaModal.tsx
+++ frontend/src/pages/Library/Media/components/CopyMediaModal.tsx
@@ -54,6 +54,7 @@ export default function CopyMediaModal({
   useEffect(() => {
     if (media && isOpen) {
       setNewName(incrementName(media.name));
+      setNewTags(media.tags ?? []);
     }
   }, [media, isOpen]);
 
