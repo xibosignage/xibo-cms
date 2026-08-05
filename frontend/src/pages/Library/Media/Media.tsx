d57f12846bd9e856c2e98b9dcb3e621eb6347ad2
Mauro Ferrão <maurofmferrao@gmail.com>
2026-08-05 14:19:33 +0100

    Filter height and scroll fix (#3771)
    relates to xibosignageltd/xibo-private#1784

--- frontend/src/pages/Library/Media/Media.tsx
+++ frontend/src/pages/Library/Media/Media.tsx
@@ -664,7 +664,7 @@ export default function Media() {
           </div>
         )}
 
-        <div className="min-h-0 flex flex-col">
+        <div className="flex-1 min-h-0 flex flex-col">
           {!isHydrated ? (
             <div className="flex-1 flex items-center justify-center bg-gray-50 animate-pulse rounded-lg border border-gray-200">
               <span className="text-gray-400 font-medium">
